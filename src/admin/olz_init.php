6bd86d60ff8a7603906567ae96a12cd0926974ed
Simon Hatt <hatt@google.com>
2020-02-03 18:20:57 +0100

    Misconfiguration error

--- src/admin/olz_init.php
+++ src/admin/olz_init.php
@@ -83,7 +83,9 @@ if (isset($button)) $$var = $button; // für alte Links bei Forumseinträgen und
 //-------------------------------------------
 // Datenbankverbindung
 //-------------------------------------------
-require_once($_SERVER['DOCUMENT_ROOT']."/config.php");
+$config_path = $_SERVER['DOCUMENT_ROOT'].'/config.php';
+if (!is_file($config_path)) die('Config file not found');
+require_once($config_path);
 if ($db->connect_error) die("Connect Error (".$db->connect_errno.") ".$db->connect_error);
 //mysql_query('SET NAMES utf8');
 $db->query("SET NAMES utf8");

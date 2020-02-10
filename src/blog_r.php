2ee5709ac0dafe8abb29529d7326ca37606a997c
OL Zimmerberg <60570337+olzimmerberg@users.noreply.github.com>
2020-02-10 22:45:04 +0100

    Fix Kaderblog (#25)
    Co-authored-by: Simon Hatt <simon+github@hatt.style>

--- src/blog_r.php
+++ src/blog_r.php
@@ -1,34 +1,36 @@
-<div class="tablebar">Elite</div>
+<?php
+
+echo "<div class='tablebar'>Elite</div>
 <div>
 <h3>Julia Jakob</h3>
-<img src="img/elite_julia.jpg" class="noborder" style="max-width: 300px;">
+<img src='{$data_href}img/elite_julia.jpg' class='noborder' style='max-width: 300px;'>
 <div>Julia Jakob ist Welt- und Europameisterin im Staffel-OL und Mitglied des A-Kaders von swiss orienteering.</div>
-<div>Homepage: <a href="http://juliajakob.ch/" class="linkext">juliajakob.ch</a></div>
-<div>Facebook: <a href="http://facebook.com/fanfuergross" class="linkext">Fan für Gross</a></div>
+<div>Homepage: <a href='http://juliajakob.ch/' class='linkext'>juliajakob.ch</a></div>
+<div>Facebook: <a href='http://facebook.com/fanfuergross' class='linkext'>Fan für Gross</a></div>
 <h3>Paula Gross</h3>
-<img src="img/elite_paula.jpg" class="noborder" style="max-width: 300px;">
+<img src='{$data_href}img/elite_paula.jpg' class='noborder' style='max-width: 300px;'>
 <div>Paula Gross ist Junioren Staffel Weltmeisterin und Mitglied des Elite Nationalkaders B.</div>
-<div>Facebook: <a href="http://facebook.com/fanfuergross" class="linkext">Fan für Gross</a></div>
+<div>Facebook: <a href='http://facebook.com/fanfuergross' class='linkext'>Fan für Gross</a></div>
 <h3>Florian Attinger</h3>
 <div>Florian Attinger ist 4-facher Silbermedaillengewinner an den Jugendeuropameisterschaften und Mitglied der Anschlussgruppe des Elite Nationalkaders.</div>
-<div>Strava: <a href="https://www.strava.com/athletes/3997545" class="linkext">Profil</a></div>
+<div>Strava: <a href='https://www.strava.com/athletes/3997545' class='linkext'>Profil</a></div>
 </div>
 <br />
-<div class="tablebar">Team Gold</div>
-<div style="line-height: 64px;"><img src="img/gold_gratian.jpg" class="noborder" style="width: 64px;"> <b>Gratian Böhi</b></div>
-<div style="line-height: 64px;"><img src="img/gold_lena.jpg" class="noborder" style="width: 64px;"> <b>Lena Gasser</b></div>
-<div style="line-height: 64px;"><img src="img/gold_lukas.jpg" class="noborder" style="width: 64px;"> <b>Lukas Gasser</b></div>
-<div style="line-height: 64px;"><img src="img/gold_silvan.jpg" class="noborder" style="width: 64px;"> <b>Silvan Ghirlanda</b></div>
-<div style="line-height: 64px;"><img src="img/gold_anna.jpg" class="noborder" style="width: 64px;"> <b>Anna Rettich</b></div>
-<div style="line-height: 64px;"><img src="img/gold_flurin.jpg" class="noborder" style="width: 64px;"> <b>Flurin Rettich</b></div>
-<div style="line-height: 64px;"><img src="img/gold_gian.jpg" class="noborder" style="width: 64px;"> <b>Gian Rettich</b></div>
+<div class='tablebar'>Team Gold</div>
+<div style='line-height: 64px;'><img src='{$data_href}img/gold_gratian.jpg' class='noborder' style='width: 64px;'> <b>Gratian Böhi</b></div>
+<div style='line-height: 64px;'><img src='{$data_href}img/gold_lena.jpg' class='noborder' style='width: 64px;'> <b>Lena Gasser</b></div>
+<div style='line-height: 64px;'><img src='{$data_href}img/gold_lukas.jpg' class='noborder' style='width: 64px;'> <b>Lukas Gasser</b></div>
+<div style='line-height: 64px;'><img src='{$data_href}img/gold_silvan.jpg' class='noborder' style='width: 64px;'> <b>Silvan Ghirlanda</b></div>
+<div style='line-height: 64px;'><img src='{$data_href}img/gold_anna.jpg' class='noborder' style='width: 64px;'> <b>Anna Rettich</b></div>
+<div style='line-height: 64px;'><img src='{$data_href}img/gold_flurin.jpg' class='noborder' style='width: 64px;'> <b>Flurin Rettich</b></div>
+<div style='line-height: 64px;'><img src='{$data_href}img/gold_gian.jpg' class='noborder' style='width: 64px;'> <b>Gian Rettich</b></div>
 <br />
-<div class="tablebar">Trainer Team Gold</div>
-<div><img src="img/gold_trainer.jpg" class="noborder" style="width: 100%;"></div>
+<div class='tablebar'>Trainer Team Gold</div>
+<div><img src='{$data_href}img/gold_trainer.jpg' class='noborder' style='width: 100%;'></div>
 <div><b>Hauptleitung</b></div>
-<script>document.write(MailTo("tiziana.rigamonti", "olzimmerberg.ch", "Tiziana Rigamonti", "Team Gold"))</script>
+<script>document.write(MailTo('tiziana.rigamonti', 'olzimmerberg.ch', 'Tiziana Rigamonti', 'Team Gold'))</script>
 <div><b>Trainerteam</b></div>
 <div>Lilly Gross</div>
 <div>Paula Gross</div>
 <div>Michael Felder</div>
-<div>Adrian Köpfli</div>
+<div>Adrian Köpfli</div>";

d9162300b68bd2119b677e1f00d4546cd9855e4a
Simon Hatt <hatt@google.com>
2020-02-21 23:50:12 +0100

    Fix flaky tests.
    SCREENSHOT_APPROVE={"karten":"all","fuer_einsteiger":"all"}

--- src/index.php
+++ src/index.php
@@ -408,6 +408,29 @@ else {
 echo "<div style='clear:both;'>&nbsp;</div></div>
 </div>
 </div>
+<script>
+function hideFlaky() {
+    var flakyElements = document.querySelectorAll('.test-flaky');
+    for (var i=0; i<flakyElements.length; i++) {
+        var rect = flakyElements[i].getBoundingClientRect();
+        var cover = document.getElementById('flaky-' + i);
+        if (!cover) {
+            var cover = document.createElement('div');
+            document.documentElement.appendChild(cover);
+            cover.id = 'flaky-' + i;
+            cover.style.position = 'absolute';
+            cover.style.backgroundColor = 'black';
+            cover.style.zIndex = 999999;
+        }
+        cover.style.width = Math.ceil(rect.width+1) + 'px';
+        cover.style.height = Math.ceil(rect.height+1) + 'px';
+        cover.style.top = Math.floor(rect.top) + 'px';
+        cover.style.left = Math.floor(rect.left) + 'px';
+    }
+}
+hideFlaky();
+setInterval(hideFlaky, 0);
+</script>
 </body>
 </html>";
 

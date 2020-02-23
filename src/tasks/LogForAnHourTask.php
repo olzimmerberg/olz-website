bc9d20fcbf1102dd5bbfaf68836734644b89a71b
Simon Hatt <hatt@google.com>
2020-02-23 21:13:59 +0100

    Report throttling errors, log time limit

--- src/tasks/LogForAnHourTask.php
+++ src/tasks/LogForAnHourTask.php
@@ -8,6 +8,12 @@ class LogForAnHourTask extends BackgroundTask {
     }
 
     protected function run_specific_task() {
+        $success = set_time_limit(4000);
+        if ($success) {
+            $this->log_info("Successfully set time limit");
+        } else {
+            $this->log_warning("Could not set time limit. Let's hope for the best :/");
+        }
         for ($i = 0; $i < 360; $i++) {
             $time = date('H:i:s');
             $this->log_info("It is {$time}");

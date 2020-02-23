7de978f150b3428092f372feda7086c7946824d9
Simon Hatt <hatt@google.com>
2020-02-24 00:08:21 +0100

    Improve deployment speed

--- build-deployment.sh
+++ build-deployment.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# DEPLOYMENT_NAME=$(date '+%Y-%m-%dT%H_%M_%S')
-DEPLOYMENT_NAME='current'
-
-rm -Rf ./deploy/$DEPLOYMENT_NAME
-mkdir -p ./deploy/$DEPLOYMENT_NAME
-cp -R ./src/* ./deploy/$DEPLOYMENT_NAME/
-mkdir -p ./deploy/$DEPLOYMENT_NAME/screenshots/generated
-echo $DEPLOYMENT_NAME >> ./deploy/CURRENT_NAME
+rm -Rf ./deploy/
+mkdir -p ./deploy/
+cp -R ./src/* ./deploy/
+mkdir -p ./deploy/screenshots/generated
+zip -r ./deploy.zip ./deploy/

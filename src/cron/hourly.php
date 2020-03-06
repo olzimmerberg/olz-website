f2e96cd7bd1cd61094c2adcaadd06798ec5e55a4
OL Zimmerberg <60570337+olzimmerberg@users.noreply.github.com>
2020-03-06 19:36:01 +0100

    Disable hourly task log spam (#58)
    Co-authored-by: Simon Hatt <hatt@google.com>

--- src/cron/hourly.php
+++ src/cron/hourly.php
@@ -4,8 +4,8 @@ require_once __DIR__.'/common.php';
 require_once __DIR__.'/../tasks/LogForAnHourTask.php';
 
 function run_hourly() {
-    $job = new LogForAnHourTask();
-    $job->run();
+    // $job = new LogForAnHourTask();
+    // $job->run();
 }
 
 throttle('cron_hourly', 'run_hourly', [], 30 * 60);

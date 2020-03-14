9bb86c8c37716be9db57679c1e401577c9a48f87
OL Zimmerberg <60570337+olzimmerberg@users.noreply.github.com>
2020-03-14 13:59:45 +0100

    Manage jQuery as a dependency (#68)
    Co-authored-by: Simon Hatt <hatt@google.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import * as jQueryTmp from 'jquery';
+
 export * from './scripts/jscripts';
 export * from './scripts/fader';
 export * from './scripts/accordion';
+
+export const jQuery = jQueryTmp;
+export const $ = jQueryTmp;
 
 export function loaded() {
     // TODO: remove this!

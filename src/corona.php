83680d7415729dc80440e786248ef575e0ca2d33
OL Zimmerberg <60570337+olzimmerberg@users.noreply.github.com>
2020-03-23 22:30:11 +0100

    OLZ Corona-Lifehacks - draft (#86)
    Co-authored-by: Simon Hatt <hatt@google.com>

---
+++ src/corona.php
@@ -0,0 +1,13 @@
+<h2 style='font-size:24px; border:0px; text-align:center;'>OLZ Corona-Lifehacks 2020</h2>
+<p style='text-align:center; font-size:15px; max-width:600px; margin:0px auto;'>Wie wir die OL-Krise überstehen</p>
+<p style='text-align:center;'>Die Versicherung ist Sache der Teilnehmenden. Der Veranstalter lehnt, soweit gesetzlich zulässig, jede Haftung ab.</p>
+
+<?php
+olz_text_insert(13);
+
+olz_text_insert(14);
+
+olz_text_insert(15);
+
+olz_text_insert(16);
+?>

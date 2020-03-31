9e6b01e1f3573fb9e3d8cde7e8e4c23f1387ab56
OL Zimmerberg <60570337+olzimmerberg@users.noreply.github.com>
2020-03-31 19:44:19 +0200

    Corona: Der Posten (#94)
    Co-authored-by: Simon Hatt <hatt@google.com>

--- src/corona.php
+++ src/corona.php
@@ -1,5 +1,6 @@
 <h2 style='font-size:24px; border:0px; text-align:center;'>OLZ Corona-Lifehacks 2020</h2>
 <p style='text-align:center; font-size:15px; max-width:600px; margin:0px auto;'>Wie wir die OL-Krise überstehen</p>
+<div class='corona'>
 
 <?php
 olz_text_insert(13);
@@ -9,4 +10,11 @@ olz_text_insert(14);
 olz_text_insert(15);
 
 olz_text_insert(16);
+
+echo "<style>.corona .buttonbar { display: none; }</style>";
+echo "<h3>Der Posten</h3>";
+echo "<p>Ideen an <script>document.write(MailTo('jan.waldmann', 'olzimmerberg.ch', 'Jan Waldmann', 'Der Posten'))</script></p>";
+$id = 1329;
+include_once __DIR__.'/galerie_l.php';
 ?>
+</div>

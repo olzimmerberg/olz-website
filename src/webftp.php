cdd6d24c91ce09b64affb5a48ddf653fcb1ab0da
Simon Hatt <hatt@google.com>
2020-06-22 18:24:28 +0200

    Individuelle Layouts für alle Seiten
    SCREENSHOT_APPROVE={"divmail-firefox":"all"}

--- src/webftp.php
+++ src/webftp.php
@@ -3,7 +3,7 @@
 echo "<div id='content_double'>
 <form name='Formularl' method='post' action='index.php#id_edit".$_SESSION['id_edit']."' enctype='multipart/form-data'>
 <div>";
-include __DIR__.'/material_d.php';
+include __DIR__.'/library/phpWebFileManager/start.php';
 echo "</div>
 </form>
 </div>";

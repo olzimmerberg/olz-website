94a45cc2cc4302897006046bcc6fb29880ab9866
Simon Hatt <hatt@google.com>
2020-07-28 12:30:07 +0200

    Fix live results

--- src/resultate/update.php
+++ src/resultate/update.php
@@ -7,32 +7,8 @@ $is_filename_ok = preg_match('/^[a-z0-9\-\.]+$/', $filename);
 if (!$is_filename_ok) {
     die('Filename must match ^[a-z0-9\-\.]+$');
 }
-$results_data_path = realpath("{$data_path}results/");
-$file_path = realpath("{$results_data_path}{$filename}");
-if (isset($_POST['diff'])) {
-    if (!is_file($file_path)) {
-        die(json_encode([false, "No such file: {$file_path}"]));
-    }
-    $diff_content = base64_decode($_POST['diff']);
-    if (!$diff_content) {
-        die(json_encode([false, "Invalid base64 data"]));
-    }
-    file_put_contents($file_path.".patch", $diff_content);
-    $res = shell_exec("patch -u ".$file_path." -i ".$file_path.".patch -o ".$file_path.".patched");
-    $patch_ok = (filesize($file_path) > 0);
-    if ($patch_ok) {
-        rename($file_path, $file_path.".bak_".date("Y-m-dTH:i:s"));
-        rename($file_path.".patched", $file_path);
-        file_put_contents(
-            "{$results_data_path}_live.json",
-            json_encode([
-                'file' => $file_path,
-                'last_updated_at' => date('Y-m-d H:i:s'),
-            ]),
-        );
-    }
-    die(json_encode([$patch_ok, $res]));
-}
+$results_data_path = realpath("{$data_path}results");
+$file_path = "{$results_data_path}/{$filename}";
 if (isset($_POST['new'])) {
     if (is_file($file_path)) {
         rename($file_path, $file_path.".bak_".date("Y-m-dTH:i:s"));
@@ -43,9 +19,9 @@ if (isset($_POST['new'])) {
     }
     file_put_contents($file_path, $new_content);
     file_put_contents(
-        "{$results_data_path}_live.json",
+        "{$results_data_path}/_live.json",
         json_encode([
-            'file' => $file_path,
+            'file' => $filename,
             'last_updated_at' => date('Y-m-d H:i:s'),
         ]),
     );

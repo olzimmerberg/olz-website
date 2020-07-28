386944e9eca015f265c7715f3e13d3485ce5bb42
OL Zimmerberg <60570337+olzimmerberg@users.noreply.github.com>
2020-07-28 12:33:19 +0200

    Fix live results (#164)
    Co-authored-by: Simon Hatt <hatt@google.com>

--- src/resultate/live_uploader/uploader/updater.py
+++ src/resultate/live_uploader/uploader/updater.py
@@ -53,26 +53,16 @@ def _run_updater(server_url, file_path):
             os.path.splitext(file_path),
         )
 
+        should_upload = True
         if new_file:
-            print("Patch: All new...")
-            with open(file_path, 'rb') as fpr:
-                with open(file_path + '.patch', 'wb+') as fpw:
-                    fpw.write(fpr.read())
-                    print("Patch: All new")
+            should_upload = True
         else:
-            print("Diff -u...")
-            with subprocess.Popen(
-                'diff' + ' ' +
-                '-u' + ' ' +
-                previous_file_path + ' ' +
-                file_path,
-                shell=True,
-                stdout=subprocess.PIPE,
-            ) as proc:
-                with open(file_path + '.patch', 'wb+') as fpw:
-                    fpw.write(proc.stdout.read())
+            with open(file_path, 'rb') as fpr:
+                with open(previous_file_path, 'rb') as fprp:
+                    should_upload = fpr.read() != fprp.read()
 
-        if new_file:
+        if should_upload:
+            print('Upload...')
             with open(file_path, 'rb') as fpr:
                 new_content = fpr.read()
                 with open(previous_file_path, 'wb+') as fpw:
@@ -87,61 +77,17 @@ def _run_updater(server_url, file_path):
                             ).encode(),
                             headers={'User-Agent': 'Mozilla'},
                         )) as fh:
-                            result = json.loads(fh.read().decode())
+                            content = fh.read().decode()
+                            result = json.loads(content)
                             if result[0]:
                                 print("Successful raw-update.")
                                 time.sleep(10)
                             else:
-                                raise Exception("Server error")
+                                raise Exception(f"Server error: {content}")
                     except Exception as exc:
                         print("Failed raw-update. No idea what to do...", server_url)
                         print(exc)
                         os.unlink(previous_file_path)
         else:
-            with open(file_path + '.patch', 'rb') as fpr:
-                diff_content = fpr.read()
-                if diff_content:
-                    print("Patch -u...")
-                    with subprocess.Popen(
-                        'patch' + ' ' +
-                        '-u' + ' ' +
-                        previous_file_path + ' ' +
-                        '-i' + ' ' +
-                        file_path + '.patch' + ' ' +
-                        '-o' + ' ' +
-                        previous_file_path + '.patched',
-                        shell=True,
-                        stdout=subprocess.PIPE,
-                    ) as proc:
-                        proc.stdout.read()
-                        os.rename(
-                            previous_file_path,
-                            backup_file_path,
-                        )
-                        os.rename(
-                            previous_file_path + '.patched',
-                            previous_file_path,
-                        )
-
-                        try:
-                            with urlopen(Request(
-                                url=server_url,
-                                method='POST',
-                                data=b'diff=' + urlencode(
-                                    base64.b64encode(diff_content),
-                                ).encode(),
-                                headers={'User-Agent': 'Mozilla'},
-                            )) as fh:
-                                result = json.loads(fh.read().decode())
-                                if result[0]:
-                                    print("Successful diff-update.")
-                                    time.sleep(10)
-                                else:
-                                    raise Exception("Server error")
-                        except Exception as exc:
-                            print("Failed diff-update!")
-                            print(exc)
-                            os.unlink(previous_file_path)
-                else:
-                    print("No difference")
+            print('No difference')
         time.sleep(10)

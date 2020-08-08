b51eea8fc05a84aa32578bc46f5c6fe24e031409
OL Zimmerberg <60570337+olzimmerberg@users.noreply.github.com>
2020-08-08 14:35:40 +0200

    Solv Sync Task von Entity Manager und Fetcher entkoppeln (#173)
    Co-authored-by: Simon Hatt <hatt@google.com>

--- src/fetchers/solv_events.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function fetch_solv_events_csv_for_year($year) {
-    $url = "https://www.o-l.ch/cgi-bin/fixtures?&year={$year}&kind=&csv=1";
-    return utf8_encode(file_get_contents($url));
-}

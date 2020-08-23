43326785cc81d3cc40f1bb0e8640dd568a8036ae
Simon Hatt <hatt@google.com>
2020-08-23 14:33:22 +0200

    Zieleinlauf-Cup: Maximalpunktzahl pro Lauf anzeigen

--- src/cron/daily.php
+++ src/cron/daily.php
@@ -4,11 +4,13 @@ require_once __DIR__.'/common.php';
 require_once __DIR__.'/../config/doctrine.php';
 require_once __DIR__.'/../fetchers/SolvFetcher.php';
 require_once __DIR__.'/../tasks/SyncSolvTask.php';
+require_once __DIR__.'/../utils/date/LiveDateUtils.php';
 
 function run_daily() {
     global $entityManager;
     $solv_fetcher = new SolvFetcher();
-    $job = new SyncSolvTask($entityManager, $solv_fetcher);
+    $date_utils = new LiveDateUtils();
+    $job = new SyncSolvTask($entityManager, $solv_fetcher, $date_utils);
     $job->setDefaultFileLogger();
     $job->run();
 }

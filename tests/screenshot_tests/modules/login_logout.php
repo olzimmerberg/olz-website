fed490aa716f635cc071a0ab581f189c3acd9f89
Simon Hatt <hatt@google.com>
2020-10-18 17:15:40 +0200

    Problem mit neuem Benutzermenu beheben
    ausserdem Versuch, die unsteten Screenshots zu flicken
    
    SCREENSHOT_APPROVE={"change_password_admin-firefox":"all","login_modal-firefox":"all","trophy-firefox":"all"}

--- tests/screenshot_tests/modules/login_logout.php
+++ tests/screenshot_tests/modules/login_logout.php
@@ -31,7 +31,7 @@ function test_login_logout($driver, $base_url) {
         );
         return $login_modal->getCssValue('opacity') == 1;
     });
-    sleep(0.2);
+    sleep(0.5);
 
     take_pageshot($driver, 'login_modal');
 

5d4cbb74377e994f246d9408270d0b13e757cab7
OL Zimmerberg <60570337+olzimmerberg@users.noreply.github.com>
2020-10-18 23:06:17 +0000

    Fix Termine (#221)
    Co-authored-by: Simon Hatt <hatt@google.com>

--- src/termine.js
+++ src/termine.js
@@ -1,24 +1,19 @@
 import {CHtoWGSlat, CHtoWGSlng} from './library/wgs84_ch1903/wgs84_ch1903';
 
-export function toggleMap(id,xkoord,ykoord) {
-    var div;
-    mapid = "map"+id;
-    div = document.getElementById(mapid);
+export function toggleMap(id, xkoord, ykoord) {
+    const mapid = "map"+id;
+    const div = document.getElementById(mapid);
+    const buttonid = "map_"+id;
+    const button = document.getElementById(buttonid);
     if (div.style.display=="none") {
         div.style.display="";
-        breite = document.getElementById('Spalte1').offsetWidth-20;
-
-        div.innerHTML=getMapHtml(xkoord, ykoord, breite);
-
-        mapid = "map_"+id;
-        div = document.getElementById(mapid);
-        div.innerHTML="<a href='' onclick=\"map('"+id+"',"+xkoord+","+ykoord+");return false;\" class='linkmap'>Karte ausblenden<\/a >"
+        const wid = document.getElementById('Spalte1').offsetWidth-20;
+        div.innerHTML=getMapHtml(xkoord, ykoord, wid);
+        button.innerHTML="<a href='' onclick=\"toggleMap('"+id+"',"+xkoord+","+ykoord+");return false;\" class='linkmap'>Karte ausblenden<\/a >"
     } else {
         div.style.display="none";
         div.innerHTML="";
-        mapid = "map_"+id;
-        div = document.getElementById(mapid);
-        div.innerHTML="<a href='' onclick=\"map('"+id+"',"+xkoord+","+ykoord+");return false;\" class='linkmap'>Karte zeigen<\/a >"
+        button.innerHTML="<a href='' onclick=\"toggleMap('"+id+"',"+xkoord+","+ykoord+");return false;\" class='linkmap'>Karte zeigen<\/a >"
     }
     return false;
 }
@@ -29,4 +24,14 @@ export function getMapHtml(xkoord, ykoord, width = 400) {
 
     // Link (im Moment wird noch auf Search.ch verlinkt, denn dort sieht man öV Haltestellen)
     return "<a href='http://map.search.ch/"+xkoord+","+ykoord+"' target='_blank'><img src='https://api.mapbox.com/styles/v1/allestuetsmerweh/ckgf9qdzm1pn319ohqghudvbz/static/pin-l+009000("+lng+","+lat+")/"+lng+","+lat+",13,0/"+width+"x300?access_token=pk.eyJ1IjoiYWxsZXN0dWV0c21lcndlaCIsImEiOiJHbG9tTzYwIn0.kaEGNBd9zMvc0XkzP70r8Q' class='noborder test-flaky' style='margin:0px;padding:0px;align:center;border:1px solid #000000;'><\/a>";
+}
+
+export function mousein(id) {
+    mouseid = id;
+    document.getElementById(id).style.color = "rgb(0,0,0)";
+}
+
+export function mouseout(id) {
+    mouseid = -1;
+    document.getElementById(id).style.color = "rgb(0,110,25)";
 }

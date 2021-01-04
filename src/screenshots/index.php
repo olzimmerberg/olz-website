6675c21b4a031f5ae06d16864dad92d508b9a4b1
Simon Hatt <hatt@google.com>
2021-01-04 18:31:28 +0100

    Fix termine filters (again)
    SCREENSHOT_APPROVE={"termine-firefox":"all","termine_new_finished-firefox":"all","termine_past-firefox":"all"}

--- src/screenshots/index.php
+++ src/screenshots/index.php
@@ -53,14 +53,35 @@ if ($master_index === null) {
 sort($screenshot_paths);
 
 echo "<div id='root' class='local'>";
+$screenshot_paths_json = json_encode($screenshot_paths);
+echo "<script>const screenshotPaths = {$screenshot_paths_json};</script>";
 foreach ($screenshot_paths as $screenshot_path) {
     $has_screenshot_id = preg_match('/^([a-z0-9\\-\\_]+)\\.png$/', $screenshot_path, $matches);
     $screenshot_id = $has_screenshot_id ? " id='{$matches[1]}'" : "";
     echo "<div class='pair'>\n";
     echo "<h2{$screenshot_id}>{$screenshot_path}</h2>\n";
-    echo "<img src='{$code_href}screenshots/generated/{$screenshot_path}' class='local' />\n";
-    echo "<img src='{$master_href}screenshots/generated/{$screenshot_path}' class='master' />\n";
+    echo "<img class='local' id='local-{$screenshot_path}' />\n";
+    echo "<img class='master' id='master-{$screenshot_path}' />\n";
     echo "<div class='after-pair'></div>\n";
     echo "</div>\n";
 }
 echo "</div>";
+echo "<script>
+function loadNext(index) {
+    if (index >= screenshotPaths.length) {
+        return;
+    }
+    const loadLocal = new Promise((resolve) => {
+        const localElem = document.getElementById('local-'+screenshotPaths[index]);
+        localElem.src = '{$code_href}screenshots/generated/' + screenshotPaths[index];
+        localElem.onload = resolve;
+    });
+    const loadMaster = new Promise((resolve) => {
+        const masterElem = document.getElementById('master-'+screenshotPaths[index]);
+        masterElem.src = '{$master_href}screenshots/generated/' + screenshotPaths[index];
+        masterElem.onload = resolve;
+    });
+    Promise.all([loadLocal, loadMaster]).then(() => loadNext(index + 1));
+}
+loadNext(0);
+</script>";

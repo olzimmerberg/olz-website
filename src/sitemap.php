7822957a367bb1ca762b401f09d7c18975b5f8a9
Simon Hatt <hatt@google.com>
2021-02-17 18:22:05 +0100

    Add aktuell and galerie entries to sitemap

--- src/sitemap.php
+++ src/sitemap.php
@@ -36,4 +36,14 @@ echo get_entry("{$base_url}service.php", 'monthly', '0.3');
 echo get_entry("{$base_url}kontakt.php", 'monthly', '0.5');
 echo get_entry("{$base_url}datenschutz.php", 'monthly', '0.1');
 
+$aktuell_ids = $entityManager->getRepository(Aktuell::class)->getAllActiveIds();
+foreach ($aktuell_ids as $aktuell_id) {
+    echo get_entry("{$base_url}aktuell.php?id={$aktuell_id}", 'monthly', '0.3');
+}
+
+$galerie_ids = $entityManager->getRepository(Galerie::class)->getAllActiveIds();
+foreach ($galerie_ids as $galerie_id) {
+    echo get_entry("{$base_url}galerie.php?id={$galerie_id}", 'monthly', '0.2');
+}
+
 echo "</urlset>\n";

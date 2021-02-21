0b7ce6b32371db4215b6ef277d463200301f0ab9
Simon Hatt <hatt@google.com>
2021-02-21 19:18:09 +0100

    Move even more files to TypeScript (#327)

--- src/scripts/upload_tools.ts
+++ src/scripts/upload_tools.ts
@@ -1,4 +1,4 @@
-export function obfuscaseForUpload(str) {
+export function obfuscaseForUpload(str: string): string {
     const iv = Math.floor(Math.random() * 0xFFFF);
     let uploadStr = '';
     let current = iv;

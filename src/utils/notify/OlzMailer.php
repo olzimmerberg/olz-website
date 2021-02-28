beef83b760bc5e58d033bc9f191322fbdfcaf9a4
Simon Hatt <hatt@google.com>
2021-02-28 16:15:30 +0100

    Schöner Lesen (HTML E-Mail)

--- src/utils/notify/OlzMailer.php
+++ src/utils/notify/OlzMailer.php
@@ -9,12 +9,32 @@ class OlzMailer extends PHPMailer {
         $user_email = $user->getEmail();
         // TODO: Check if verified?
         $user_full_name = $user->getFullName();
+        $user_first_name = $user->getFirstName();
         $this->addAddress($user_email, $user_full_name);
         $html_text = nl2br($text);
         $this->isHTML(true);
+        $this->AddEmbeddedImage(__DIR__.'/../../icns/olz_logo_schwarzweiss_300.png', 'olz_logo');
         $this->Subject = "[OLZ] {$title}";
-        $this->Body = "HTML-<b>Test</b>,<br />\n{$html_text}";
-        $this->AltBody = "{$text}";
+        $this->Body = <<<ZZZZZZZZZZ
+        <div style="text-align: right; float: right;">
+            <img src="cid:olz_logo" alt="" style="width:150px;" />
+        </div>
+        Hallo <b>{$user_first_name}</b>,<br />
+        {$html_text}<br />
+        <br />
+        <hr style="border: 0; border-top: 1px solid black;">
+        Abmelden? <a href="https://olzimmerberg.ch/TODO">Keine solchen E-Mails mehr</a> - <a href="https://olzimmerberg.ch/TODO">Keine E-Mails von OL Zimmerberg mehr</a>
+        ZZZZZZZZZZ;
+        $this->AltBody = <<<ZZZZZZZZZZ
+        Hallo {$user_first_name},
+
+        {$text}
+
+        ---
+        Abmelden?
+        Keine solchen E-Mails mehr: https://olzimmerberg.ch/TODO
+        Keine E-Mails von OL Zimmerberg mehr: https://olzimmerberg.ch/TODO
+        ZZZZZZZZZZ;
     }
 
     public function send() {

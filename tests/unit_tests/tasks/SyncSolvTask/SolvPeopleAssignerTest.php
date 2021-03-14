3975c57b0b6086f2fcb0b661a55d5bd8ffb2d843
Simon Hatt <hatt@google.com>
2021-03-14 15:37:53 +0100

    Fix & test SolvPeopleAssigner (#350)

--- tests/unit_tests/tasks/SyncSolvTask/SolvPeopleAssignerTest.php
+++ tests/unit_tests/tasks/SyncSolvTask/SolvPeopleAssignerTest.php
@@ -107,6 +107,179 @@ class FakeSolvPeopleAssignerSolvResultRepository {
  * @covers \SolvPeopleAssigner
  */
 final class SolvPeopleAssignerTest extends TestCase {
+    public function testGetDifferenceBetweenPersonInfo(): void {
+        $entity_manager = new FakeSolvPeopleAssignerEntityManager();
+        $logger = new Logger('SolvPeopleAssignerTest');
+        // $logger->pushHandler(new Monolog\Handler\StreamHandler('php://stdout', Logger::INFO));
+
+        $job = new SolvPeopleAssigner($entity_manager);
+        $job->setLogger($logger);
+
+        $this->assertSame(0, $job->getDifferenceBetweenPersonInfo(
+            'Test', '07', 'Uster',
+            'Test', '07', 'Uster',
+        ));
+        $this->assertSame(1, $job->getDifferenceBetweenPersonInfo(
+            'Best', '07', 'Uster',
+            'Test', '07', 'Uster',
+        ));
+        $this->assertSame(3, $job->getDifferenceBetweenPersonInfo(
+            'Basti', '07', 'Uster',
+            'Test', '07', 'Uster',
+        ));
+        $this->assertSame(0, $job->getDifferenceBetweenPersonInfo(
+            'Test', '07', 'Uster',
+            'Test', 7, 'Uster',
+        ));
+        $this->assertSame(1, $job->getDifferenceBetweenPersonInfo(
+            'Test', '07', 'Uster',
+            'Test', '08', 'Uster',
+        ));
+        $this->assertSame(2, $job->getDifferenceBetweenPersonInfo(
+            'Test', '07', 'Uster',
+            'Test', '07', 'Muster',
+        ));
+        $this->assertSame(7, $job->getDifferenceBetweenPersonInfo(
+            'Test', '07', 'Thalwil',
+            'Test', '07', 'Muster',
+        ));
+        $this->assertSame(2, $job->getDifferenceBetweenPersonInfo(
+            'Test', '07', 'Thalwil',
+            'Test', '07', '',
+        ));
+    }
+
+    public function testGetClosestMatchesOfPersonInfo(): void {
+        $entity_manager = new FakeSolvPeopleAssignerEntityManager();
+        $logger = new Logger('SolvPeopleAssignerTest');
+        // $logger->pushHandler(new Monolog\Handler\StreamHandler('php://stdout', Logger::INFO));
+
+        $job = new SolvPeopleAssigner($entity_manager);
+        $job->setLogger($logger);
+
+        $this->assertSame([
+            'difference' => 0,
+            'matches' => [
+                ['name' => 'Test', 'birth_year' => '07', 'domicile' => 'Uster'],
+            ],
+        ], $job->getClosestMatchesOfPersonInfo(
+            'Test', '07', 'Uster',
+            [
+                ['name' => 'Test', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['name' => 'Best', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['name' => 'Test', 'birth_year' => '08', 'domicile' => 'Uster'],
+                ['name' => 'Test', 'birth_year' => '07', 'domicile' => 'Oster'],
+            ]
+        ));
+
+        $this->assertSame([
+            'difference' => 1,
+            'matches' => [
+                ['name' => 'Best', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['name' => 'Test', 'birth_year' => '08', 'domicile' => 'Uster'],
+                ['name' => 'Text', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['name' => 'Test', 'birth_year' => '07', 'domicile' => 'Oster'],
+            ],
+        ], $job->getClosestMatchesOfPersonInfo(
+            'Test', '07', 'Uster',
+            [
+                ['name' => 'Best', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['name' => 'Test', 'birth_year' => '08', 'domicile' => 'Uster'],
+                ['name' => 'Text', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['name' => 'Test', 'birth_year' => '07', 'domicile' => 'Oster'],
+            ]
+        ));
+
+        $this->assertSame([
+            'difference' => 1,
+            'matches' => [
+                ['name' => 'Best', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['name' => 'Test', 'birth_year' => '08', 'domicile' => 'Uster'],
+            ],
+        ], $job->getClosestMatchesOfPersonInfo(
+            'Test', '07', 'Uster',
+            [
+                ['name' => 'Best', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['name' => 'Text', 'birth_year' => '07', 'domicile' => 'Muster'],
+                ['name' => 'Test', 'birth_year' => '08', 'domicile' => 'Uster'],
+                ['name' => 'Best', 'birth_year' => '08', 'domicile' => 'Uster'],
+            ]
+        ));
+    }
+
+    public function testGetUnambiguousPerson(): void {
+        $entity_manager = new FakeSolvPeopleAssignerEntityManager();
+        $logger = new Logger('SolvPeopleAssignerTest');
+        // $logger->pushHandler(new Monolog\Handler\StreamHandler('php://stdout', Logger::INFO));
+
+        $job = new SolvPeopleAssigner($entity_manager);
+        $job->setLogger($logger);
+
+        $this->assertSame(null, $job->getUnambiguousPerson([
+            ['person' => 1],
+            ['person' => 2],
+            ['person' => 3],
+            ['person' => 4],
+        ]));
+        $this->assertSame(1, $job->getUnambiguousPerson([
+            ['person' => 1],
+            ['person' => 1],
+            ['person' => 1],
+            ['person' => 1],
+        ]));
+        $this->assertSame(4, $job->getUnambiguousPerson([
+            ['person' => 4],
+        ]));
+        $this->assertSame(null, $job->getUnambiguousPerson([]));
+    }
+
+    public function testGetMatchingPerson(): void {
+        $entity_manager = new FakeSolvPeopleAssignerEntityManager();
+        $logger = new Logger('SolvPeopleAssignerTest');
+        // $logger->pushHandler(new Monolog\Handler\StreamHandler('php://stdout', Logger::INFO));
+
+        $job = new SolvPeopleAssigner($entity_manager);
+        $job->setLogger($logger);
+
+        // There is one perfect match.
+        $this->assertSame(2, $job->getMatchingPerson(
+            'Test', '07', 'Uster',
+            [
+                ['person' => 1, 'name' => 'Best', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['person' => 2, 'name' => 'Test', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['person' => 3, 'name' => 'Test', 'birth_year' => '08', 'domicile' => 'Uster'],
+                ['person' => 4, 'name' => 'Test', 'birth_year' => '07', 'domicile' => 'Oster'],
+            ]
+        ));
+
+        // There are multiple matches for the same person.
+        $this->assertSame(3, $job->getMatchingPerson(
+            'Test', '07', 'Uster',
+            [
+                ['person' => 3, 'name' => 'Best', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['person' => 3, 'name' => 'Test', 'birth_year' => '08', 'domicile' => 'Uster'],
+            ]
+        ));
+
+        // There are matches, but they are for different persons.
+        $this->assertSame(null, $job->getMatchingPerson(
+            'Test', '07', 'Uster',
+            [
+                ['person' => 1, 'name' => 'Best', 'birth_year' => '07', 'domicile' => 'Uster'],
+                ['person' => 2, 'name' => 'Test', 'birth_year' => '08', 'domicile' => 'Uster'],
+            ]
+        ));
+
+        // The matches are too bad.
+        $this->assertSame(null, $job->getMatchingPerson(
+            'Test', '07', 'Uster',
+            [
+                ['person' => 1, 'name' => 'Best', 'birth_year' => '08', 'domicile' => 'Oster'],
+                ['person' => 1, 'name' => 'Text', 'birth_year' => '07', 'domicile' => 'Muster'],
+            ]
+        ));
+    }
+
     public function testSolvPeopleAssigner(): void {
         $entity_manager = new FakeSolvPeopleAssignerEntityManager();
         $logger = new Logger('SolvPeopleAssignerTest');

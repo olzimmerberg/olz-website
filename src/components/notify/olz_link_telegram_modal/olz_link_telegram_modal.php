706b2f0ec2f7c87b07f25da9a94be1792ab403a8
Simon Hatt <hatt@google.com>
2021-03-15 00:11:32 +0100

    Fix telegram with no chat id, update instructions (#356)

--- src/components/notify/olz_link_telegram_modal/olz_link_telegram_modal.php
+++ src/components/notify/olz_link_telegram_modal/olz_link_telegram_modal.php
@@ -8,10 +8,19 @@ function olz_link_telegram_modal($args = []): string {
 
     $user_agent_utils = getUserAgentUtilsFromEnv();
 
-    $install_instructions = "<li>Installiere die Telegram-App auf deinem Smartphone</li>";
+    $install_instructions = [
+        "<li>Installiere die Telegram-App auf deinem Smartphone</li>",
+        "<li>Logge dich in <a href='https://web.telegram.org' target='_blank'>Telegram Web</a> ein</li>",
+        "<li><span id='chat-link-wait'>Bitte warten...</span><span id='chat-link-ready'>Öffne <a href='' target='_blank' id='telegram-chat-link'>deinen persönlichen <b>OLZ-Info-Chat</b></a> und <b>klicke auf &quot;OPEN IN WEB&quot;</b></span></li>",
+    ];
     if ($user_agent_utils->isAndroidDevice() || $user_agent_utils->isIOsDevice()) {
-        $install_instructions = "<li><a href='https://telegram.org/dl/' target='_blank'>Installiere die Telegram-App</a></li>";
+        $install_instructions = [
+            "<li><a href='https://telegram.org/dl/' target='_blank'>Installiere die Telegram-App</a></li>",
+            "<li><span id='chat-link-wait'>Bitte warten...</span><span id='chat-link-ready'>Öffne <a href='' target='_blank' id='telegram-chat-link'>deinen persönlichen <b>OLZ-Info-Chat</b></a> und <b>klicke auf &quot;SEND MESSAGE&quot;</b></span></li>",
+        ];
     }
+
+    $install_instructions_string = implode("\n", $install_instructions);
 
     return <<<ZZZZZZZZZZ
     <div class='modal fade' id='link-telegram-modal' tabindex='-1' aria-labelledby='link-telegram-modal-label' aria-hidden='true'>
@@ -30,11 +39,7 @@ function olz_link_telegram_modal($args = []): string {
                         </div>
                     </center>
                     <ol class='todo-list'>
-                        {$install_instructions}
-                        <li>
-                            <span id='chat-link-wait'>Bitte warten...</span>
-                            <span id='chat-link-ready'>Öffne <a href='' target='_blank' id='telegram-chat-link'>deinen persönlichen OLZ-Info-Chat</a></span>
-                        </li>
+                        {$install_instructions_string}
                     </ol>
                 </div>
                 <div class='modal-footer'>

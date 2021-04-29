be35b968f23cdaaa279950c97016800309987f2b
Simon Hatt <hatt@google.com>
2021-04-29 20:04:42 +0200

    Make sure `kill -9`s actually happen

--- run.sh
+++ run.sh
@@ -2,15 +2,20 @@
 
 set -e
 
+# Start webpack file watcher
 npm run webpack-watch &
 WEBPACK_WATCH_PID=$!
 echo "Webpack Watch PID: $WEBPACK_WATCH_PID"
 
+# Start Telegram webhook simulator
 php ./tools/notify/telegram_webhook_simulator.php 127.0.0.1:30270 &
 TELEGRAM_WEBHOOK_SIMULATOR_PID=$!
 echo "Telegram Webhook Simulator PID: $TELEGRAM_WEBHOOK_SIMULATOR_PID"
 
+# Run dev server, allow aborting
+set +e
 php -S 127.0.0.1:30270 -t ./dev-server/
 
+# Clean up
 kill -9 $TELEGRAM_WEBHOOK_SIMULATOR_PID
 kill -9 $WEBPACK_WATCH_PID

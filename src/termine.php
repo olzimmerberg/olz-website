2ecd78103aa2351aa13c9ef9eaf64cf9b6fc582e
Simon Hatt <hatt@google.com>
2021-05-10 12:22:41 +0200

    Fix termine GET params (#455)

--- src/termine.php
+++ src/termine.php
@@ -10,6 +10,7 @@ session_start_if_cookie_set();
 
 require_once __DIR__.'/admin/olz_functions.php';
 require_once __DIR__.'/components/page/olz_header/olz_header.php';
+require_once __DIR__.'/fields/IntegerField.php';
 require_once __DIR__.'/fields/StringField.php';
 require_once __DIR__.'/utils/client/HttpUtils.php';
 require_once __DIR__.'/utils/env/EnvUtils.php';
@@ -21,6 +22,7 @@ $http_utils = HttpUtils::fromEnv();
 $http_utils->setLogger($logger);
 $validated_get_params = $http_utils->validateGetParams([
     new StringField('filter', ['allow_null' => true]),
+    new IntegerField('id', ['allow_null' => true]),
     new StringField('buttontermine', ['allow_null' => true]),
 ], $_GET);
 

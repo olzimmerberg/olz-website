8fe554772b114d8c3194116dbf71d4d65e629cf4
Simon Hatt <hatt@google.com>
2021-05-28 18:15:08 +0200

    Show O-L link for linked termine (#470)
    SCREENSHOT_APPROVE={"termine-firefox":"all","termine_new_edit-firefox":"all","termine_new_finished-firefox":"all"}

--- .php_cs_config.php
+++ .php_cs_config.php
@@ -7,21 +7,22 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__.'/')
 ;
 
-return PhpCsFixer\Config::create()
+$config = new PhpCsFixer\Config();
+return $config
     ->setRules([
         '@DoctrineAnnotation' => true,
         '@PSR1' => true,
         '@PSR2' => true,
         '@PhpCsFixer' => true,
         '@Symfony' => true,
-        'blank_line_before_statement' => null,
+        'blank_line_before_statement' => false,
         'braces' => [
             'position_after_functions_and_oop_constructs' => 'same',
         ],
         'increment_style' => [
             'style' => 'post',
         ],
-        'single_quote' => null,
+        'single_quote' => false,
         'yoda_style' => [
             'always_move_variable' => false,
             'equal' => false,

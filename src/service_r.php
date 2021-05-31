c9960218b0cee2ee4e3075846b90280321eaafd7
Simon Hatt <hatt@google.com>
2021-05-31 11:42:51 +0200

    More infos about OLZ-Konto
    SCREENSHOT_APPROVE={"downloads_new_edit-firefox":"all","downloads_new_finished-firefox":"all","downloads_new_preview-firefox":"all","links_new_edit-firefox":"all","links_new_finished-firefox":"all","links_new_preview-firefox":"all","newsletter_form-firefox":"all","newsletter_preview-firefox":"all","newsletter_saved-firefox":"all","service-firefox":"all","service_authenticated-firefox":"all","service_modified-firefox":"all"}

--- src/service_r.php
+++ src/service_r.php
@@ -9,8 +9,16 @@ require_once __DIR__.'/components/common/olz_editable_text/olz_editable_text.php
 ?>
 <div><b>PC-Konto: 85-256448-8</b></div>
 
-<h2>Neuer Newsletter (2021)</h2>
-<a href="konto_passwort.php" class="linkint">Erstelle ein OLZ-Konto</a>, um den neuen Newsletter erhalten zu können. Sobald man eingeloggt ist, kann man hier auf der Service-Seite den neuen Newsletter konfigurieren.
+<h2>OLZ-Konto: Neuer Newsletter</h2>
+<p>
+    <a href="konto_passwort.php" class="linkint">Erstelle ein OLZ-Konto</a>, um den neuen Newsletter erhalten zu können. Sobald man eingeloggt ist, kann man hier auf der Service-Seite den neuen Newsletter konfigurieren.
+</p>
+<p>Weitere Vorteile des OLZ-Kontos:</p>
+<ul class="bullet-list">
+    <li>Neue Galerien von dir werden sofort freigeschaltet.</li>
+    <li>Man kann zusätzlich (oder anstatt) dem E-Mail Newsletter die Infos auch per Chat-App <a href="fragen_und_antworten.php#weshalb-telegram-push" target="_blank">(zurzeit nur Telegram möglich)</a> erhalten.</li>
+    <li>Weitere Funktionen sind <a href="https://github.com/olzimmerberg/olz-website/issues/205" target="_blank">in Planung</a></li>
+</ul>
 
 <h2>Alter Newsletter</h2>
 <p style="color:rgb(200,0,0);">(wird voraussichtlich Ende 2021 abgestellt)</p>

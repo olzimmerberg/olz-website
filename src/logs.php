b8d6342df38fd982ba3c2d54d3856decfa5918e1
Simon Hatt <hatt@google.com>
2021-06-14 16:10:10 +0200

    Filter by log levels (#489)

--- src/logs.php
+++ src/logs.php
@@ -33,9 +33,22 @@ ZZZZZZZZZZ;
 
 echo "<div id='content_double'>";
 if ($user && $user->getZugriff() == 'all') {
-    echo "<center><button type='button' class='btn btn-outline-primary' onclick='olzLogsGetNextLog()'>Ältere laden</button></center>";
-    echo "<div id='logs'></div>";
-    echo "<script>olzLogsGetFirstLog();</script>";
+    echo <<<'ZZZZZZZZZZ'
+    <div class='logs-header'>
+        <button type='button' class='form-control btn btn-outline-primary' onclick='olzLogsGetNextLog()'>
+            Ältere laden
+        </button>
+        <select id='log-level-filter-select' class='form-control' onchange='olzLogsLevelFilterChange()'>
+            <option value='levels-all' selected>Alle Log-Levels</option>
+            <option value='levels-info-higher'>"Info" & höher</option>
+            <option value='levels-notice-higher'>"Notice" & höher</option>
+            <option value='levels-warning-higher'>"Warning" & höher</option>
+            <option value='levels-error-higher'>"Error" & höher</option>
+        </select>
+    </div>
+    <div id='logs'></div>
+    <script>olzLogsGetFirstLog();</script>
+    ZZZZZZZZZZ;
 } else {
     echo "<div id='profile-message' class='alert alert-danger' role='alert'>Kein Zugriff!</div>";
 }

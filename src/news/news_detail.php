8e015266643f0cce04c6e8c5b480d66ff5034d5f
Simon Hatt <hatt@google.com>
2021-06-28 09:44:36 +0200

    Restructure news/
    SCREENSHOT_APPROVE={"aktuell_new_preview-firefox":"all","aktuell_update_preview-firefox":"all"}

---
+++ src/news/news_detail.php
@@ -0,0 +1,101 @@
+<?php
+
+// =============================================================================
+// Aktuelle Berichte von offiziellen Vereinsorganen.
+// =============================================================================
+
+require_once __DIR__.'/../config/paths.php';
+require_once __DIR__.'/../config/database.php';
+require_once __DIR__.'/../config/date.php';
+
+$article_metadata = "";
+try {
+    require_once __DIR__.'/components/olz_article_metadata/olz_article_metadata.php';
+    $article_metadata = olz_article_metadata($id);
+} catch (Exception $exc) {
+    $http_utils->dieWithHttpError(404);
+}
+
+echo olz_header([
+    'title' => "Aktuell",
+    'description' => "Aktuelle Beiträge, Berichte von Anlässen und weitere Neuigkeiten von der OL Zimmerberg.",
+    'additional_headers' => [
+        $article_metadata,
+    ],
+]);
+
+$button_name = 'button'.$db_table;
+if (isset($_GET[$button_name])) {
+    $_POST[$button_name] = $_GET[$button_name];
+}
+if (isset($_POST[$button_name])) {
+    $_SESSION['edit']['db_table'] = $db_table;
+}
+
+$zugriff = ((($_SESSION['auth'] ?? null) == 'all') or (in_array($db_table, preg_split('/ /', $_SESSION['auth'] ?? '')))) ? '1' : '0';
+
+$sql = "SELECT * FROM {$db_table} WHERE (id = '{$id}') ORDER BY datum DESC";
+$result = $db->query($sql);
+$row = $result->fetch_assoc();
+$pretty_date = $_DATE->olzDate("tt.mm.jjjj", $row['datum']);
+$pretty_author = $row['autor'];
+
+$id_edit = $_SESSION['id_edit'] ?? ''; // TODO: Entfernen?
+echo <<<ZZZZZZZZZZ
+<div id='content_rechts' class='optional'>
+    <div style='padding:4px 3px 10px 3px;'>
+        <b>Datum: </b>{$pretty_date}<br />
+        <b>Autor: </b>{$pretty_author}
+    </div>
+</div>
+<div id='content_mitte'>
+<form name='Formularl' method='post' action='aktuell.php?id={$id}#id_edit{$id_edit}' enctype='multipart/form-data'>
+ZZZZZZZZZZ;
+
+//-------------------------------------------------------------
+// DATENSATZ EDITIEREN
+if ($zugriff) {
+    $functions = ['neu' => 'Neuer Eintrag',
+        'edit' => 'Bearbeiten',
+        'abbruch' => 'Abbrechen',
+        'vorschau' => 'Vorschau',
+        'save' => 'Speichern',
+        'delete' => 'Löschen',
+        'start' => 'start',
+        'upload' => 'Upload',
+        'deletebild1' => 'BILD 1 entfernen',
+        'deletebild2' => 'BILD 2 entfernen',
+        'deletebild3' => 'BILD 3 entfernen',
+        'undo' => 'undo', ];
+} else {
+    $functions = [];
+}
+$function = array_search($_POST[$button_name] ?? null, $functions);
+if ($function != "") {
+    include __DIR__.'/../admin/admin_db.php';
+}
+if (($_SESSION['edit']['table'] ?? null) == $db_table) {
+    $db_edit = "1";
+} else {
+    $db_edit = "0";
+}
+
+//-------------------------------------------------------------
+// MENÜ
+if ($zugriff and ($db_edit == '0')) {
+    echo "<div class='buttonbar'>\n".olz_buttons("button".$db_table, [["Neuer Eintrag", "0"]], "")."</div>";
+}
+
+//-------------------------------------------------------------
+// AKTUELL - VORSCHAU
+if (($db_edit == "0") or (($do ?? null) == 'vorschau')) {
+    require_once __DIR__.'/components/olz_news_article/olz_news_article.php';
+    echo olz_news_article([
+        'id' => $id,
+        'can_edit' => $zugriff,
+        'is_preview' => (($do ?? null) == 'vorschau'),
+    ]);
+}
+
+echo "</form>
+</div>";
---
+++ src/news/news_list.php
@@ -0,0 +1,109 @@
+<?php
+
+require_once __DIR__.'/../components/common/olz_posting_list_item/olz_posting_list_item.php';
+require_once __DIR__.'/../utils/NewsUtils.php';
+require_once __DIR__.'/components/olz_news_filter/olz_news_filter.php';
+
+$news_utils = NewsUtils::fromEnv();
+$current_filter = json_decode($_GET['filter'] ?? '{}', true);
+
+if (!$news_utils->isValidFilter($current_filter)) {
+    $enc_json_filter = urlencode(json_encode($news_utils->getDefaultFilter()));
+    $http_utils->redirect("?filter={$enc_json_filter}", 308);
+}
+
+echo olz_header([
+    'title' => "Aktuell",
+    'description' => "Aktuelle Beiträge, Berichte von Anlässen und weitere Neuigkeiten von der OL Zimmerberg.",
+]);
+
+echo "<div id='content_rechts'>";
+echo "<h2>Filter</h2>";
+echo olz_news_filter([]);
+echo "</div>";
+echo "<div id='content_mitte'>";
+echo "<form method='post' action='aktuell.php#id_edit".($_SESSION['id_edit'] ?? '')."' enctype='multipart/form-data'>";
+
+//-------------------------------------------------------------
+// DATENSATZ EDITIEREN
+if ($zugriff) {
+    $functions = [
+        'neu' => 'Neuer Eintrag',
+        'edit' => 'Bearbeiten',
+        'abbruch' => 'Abbrechen',
+        'vorschau' => 'Vorschau',
+        'save' => 'Speichern',
+        'delete' => 'Löschen',
+        'start' => 'start',
+        'upload' => 'Upload',
+        'deletebild1' => 'BILD 1 entfernen',
+        'deletebild2' => 'BILD 2 entfernen',
+        'deletebild3' => 'BILD 3 entfernen',
+        'undo' => 'undo',
+    ];
+} else {
+    $functions = [];
+}
+$function = array_search($_POST[$button_name] ?? null, $functions);
+if ($function != "") {
+    include __DIR__.'/../admin/admin_db.php';
+}
+if (($_SESSION['edit']['table'] ?? null) == $db_table) {
+    $db_edit = "1";
+} else {
+    $db_edit = "0";
+}
+
+//-------------------------------------------------------------
+// MENÜ
+if ($zugriff and ($db_edit == '0')) {
+    echo "<div class='buttonbar'>\n".olz_buttons("button".$db_table, [["Neuer Eintrag", "0"]], "")."</div>";
+}
+
+if (($do ?? null) == 'vorschau') {
+    $id = $_SESSION[$db_table."id"];
+    require_once __DIR__.'/components/olz_news_article/olz_news_article.php';
+    echo olz_news_article([
+        'id' => $id,
+        'row' => $vorschau,
+        'can_edit' => $zugriff,
+        'is_preview' => true,
+    ]);
+    echo "<hr /><br/><br/>";
+}
+
+$filter_where = $news_utils->getSqlFromFilter($current_filter);
+$sql = <<<ZZZZZZZZZZ
+SELECT
+    id,
+    datum,
+    zeit,
+    titel,
+    text
+FROM aktuell n
+WHERE
+    {$filter_where}
+    AND n.on_off='1'
+    AND n.typ NOT LIKE 'box%'
+ORDER BY datum DESC, zeit DESC
+ZZZZZZZZZZ;
+$res = $db->query($sql);
+
+while ($row = $res->fetch_assoc()) {
+    $icon = "{$code_href}icns/entry_type_aktuell_20.svg";
+    $datum = $row['datum'];
+    $title = $row['titel'];
+    $text = $row['text'];
+    $link = "?id=".$row['id'];
+
+    echo olz_posting_list_item([
+        'icon' => $icon,
+        'date' => $datum,
+        'title' => $title,
+        'text' => $text,
+        'link' => $link,
+    ]);
+}
+
+echo "</form>";
+echo "</div>";

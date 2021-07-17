f1bfeb345ea34df6bc33779ce5e7f4d3153e55f4
Simon Hatt <hatt@google.com>
2021-07-17 00:54:33 +0000

    Fix hiding flaky elements (#535)
    SCREENSHOT_APPROVE={"bild_der_woche_new_edit-firefox":"all","bild_der_woche_new_finished-firefox":"all","bild_der_woche_new_preview-firefox":"all","live_results_link-firefox":"all","login_account_menu-firefox":"all","login_modal-firefox":"all","logout_account_menu-firefox":"all","startseite-firefox":"all","startseite_banner_edit-firefox":"all","startseite_banner_finished-firefox":"all","startseite_bild_der_woche-firefox":"all","trophy-firefox":"all"}

--- tests/screenshot_tests/utils/hideFlaky.js
+++ tests/screenshot_tests/utils/hideFlaky.js
@@ -1,28 +1,28 @@
-const flakyElements = document.querySelectorAll('.test-flaky');
-for (let i = 0; i < flakyElements.length; i++) {
-    const rect = flakyElements[i].getBoundingClientRect();
-    let cover = document.getElementById(`flaky-${i}`);
-    if (!cover) {
-        cover = document.createElement('div');
+function hideFlaky() {
+    const flakyElements = document.querySelectorAll('.test-flaky');
+    for (let i = 0; i < flakyElements.length; i++) {
+        const rect = flakyElements[i].getBoundingClientRect();
+        const cover = document.createElement('div');
         document.documentElement.appendChild(cover);
         cover.id = `flaky-${i}`;
-        cover.style.position = 'fixed';
+        cover.style.position = 'absolute';
         cover.style.backgroundColor = 'black';
         cover.style.pointerEvents = 'none';
         cover.style.zIndex = 999999;
+        cover.style.width = `${Math.ceil(rect.width + 1)}px`;
+        cover.style.height = `${Math.ceil(rect.height + 1)}px`;
+        cover.style.top = `${Math.floor(rect.top)}px`;
+        cover.style.left = `${Math.floor(rect.left)}px`;
     }
-    cover.style.width = `${Math.ceil(rect.width + 1)}px`;
-    cover.style.height = `${Math.ceil(rect.height + 1)}px`;
-    cover.style.top = `${Math.floor(rect.top)}px`;
-    cover.style.left = `${Math.floor(rect.left)}px`;
-}
 
-const inputElements = document.querySelectorAll('input');
-for (let i = 0; i < inputElements.length; i++) {
-    inputElements[i].setAttribute('spellcheck', 'false');
-}
+    const inputElements = document.querySelectorAll('input');
+    for (let i = 0; i < inputElements.length; i++) {
+        inputElements[i].setAttribute('spellcheck', 'false');
+    }
 
-const textareaElements = document.querySelectorAll('textarea');
-for (let i = 0; i < textareaElements.length; i++) {
-    textareaElements[i].setAttribute('spellcheck', 'false');
+    const textareaElements = document.querySelectorAll('textarea');
+    for (let i = 0; i < textareaElements.length; i++) {
+        textareaElements[i].setAttribute('spellcheck', 'false');
+    }
 }
+hideFlaky();

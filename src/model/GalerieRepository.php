8ab35dbd8cf9e525d220987178851138052162a5
Simon Hatt <hatt@google.com>
2021-07-25 13:57:00 +0200

    Include only pages to be indexed in sitemap

--- src/model/GalerieRepository.php
+++ src/model/GalerieRepository.php
@@ -7,10 +7,14 @@ require_once __DIR__.'/../config/doctrine.php';
 
 class GalerieRepository extends EntityRepository {
     public function getAllActiveIds() {
+        global $_DATE;
+        require_once __DIR__.'/../config/date.php';
+        $five_years_ago = $_DATE->getCurrentDateInFormat('Y') - 5;
+        $beginning_of_five_years_ago = "{$five_years_ago}-01-01";
         $dql = "
             SELECT ga.id
             FROM Galerie ga
-            WHERE ga.on_off='1'";
+            WHERE ga.on_off='1' AND ga.datum>='{$beginning_of_five_years_ago}'";
         $query = $this->getEntityManager()->createQuery($dql);
         $result = $query->getResult();
         return array_map(function ($obj) {

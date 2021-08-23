23568fcf988268a15ff2120c92fbba659c39d90c
Simon Hatt <hatt@google.com>
2021-08-23 16:04:13 +0000

    Show IDs of roles in Organigramm to admin users (#553)

--- src/components/verein/olz_organigramm/olz_organigramm.php
+++ src/components/verein/olz_organigramm/olz_organigramm.php
@@ -3,10 +3,14 @@
 function olz_organigramm($args = []): string {
     global $entityManager;
 
+    require_once __DIR__.'/../../../components/users/olz_user_info_with_popup/olz_user_info_with_popup.php';
     require_once __DIR__.'/../../../config/paths.php';
     require_once __DIR__.'/../../../config/doctrine_db.php';
     require_once __DIR__.'/../../../model/index.php';
-    require_once __DIR__.'/../../../components/users/olz_user_info_with_popup/olz_user_info_with_popup.php';
+    require_once __DIR__.'/../../../utils/auth/AuthUtils.php';
+
+    $auth_utils = AuthUtils::fromEnv();
+    $has_all_permissions = $auth_utils->hasPermission('all');
 
     $role_repo = $entityManager->getRepository(Role::class);
     $user_repo = $entityManager->getRepository(User::class);
@@ -17,9 +21,10 @@ function olz_organigramm($args = []): string {
     foreach ($root_roles as $root_role) {
         $root_role_name = nl2br($root_role->getName());
         $root_role_username = $root_role->getUsername();
+        $root_role_id = ($has_all_permissions ? "<br/>(ID: {$root_role->getId()})" : '');
         $org .= "<td style='width:".$colwid."px; vertical-align:top;'>";
         $org .= "<div id='link-role-{$root_role->getId()}' style='margin:0px 0px 0px 1px; padding:0px; border:1px solid #000000; text-align:center;'>";
-        $org .= "<h6 style='font-weight:bold; min-height:36px;'><a href='verein.php?ressort={$root_role_username}'>{$root_role_name}</a></h6>";
+        $org .= "<h6 style='font-weight:bold; min-height:36px;'><a href='verein.php?ressort={$root_role_username}'>{$root_role_name}</a>{$root_role_id}</h6>";
         $root_role_assignees = $root_role->getUsers();
         foreach ($root_role_assignees as $root_role_assignee) {
             $org .= olz_user_info_with_popup($root_role_assignee, 'name_picture');
@@ -29,9 +34,10 @@ function olz_organigramm($args = []): string {
         foreach ($charge_roles as $charge_role) {
             $charge_role_name = nl2br($charge_role->getName());
             $charge_role_username = nl2br($charge_role->getUsername());
+            $charge_role_id = ($has_all_permissions ? "<br/>(ID: {$charge_role->getId()})" : '');
             $org .= "<div style='text-align:center; height:20px; overflow:hidden;'><span style='border-left:1px solid #000000; font-size:20px;'></span></div>";
             $org .= "<div id='link-role-{$charge_role->getId()}' style='margin:0px 0px 0px 1px; padding:0px; border:1px solid #000000; text-align:center;'>";
-            $org .= "<h6 style='font-weight:bold;'><a href='verein.php?ressort={$charge_role_username}'>{$charge_role_name}</a></h6>";
+            $org .= "<h6 style='font-weight:bold;'><a href='verein.php?ressort={$charge_role_username}'>{$charge_role_name}</a>{$charge_role_id}</h6>";
             $charge_role_assignees = $charge_role->getUsers();
             foreach ($charge_role_assignees as $charge_role_assignee) {
                 $org .= olz_user_info_with_popup($charge_role_assignee, 'name');
@@ -40,7 +46,8 @@ function olz_organigramm($args = []): string {
             foreach ($subcharge_roles as $subcharge_role) {
                 $subcharge_role_name = nl2br($subcharge_role->getName());
                 $subcharge_role_username = nl2br($subcharge_role->getUsername());
-                $org .= "<div id='link-role-{$subcharge_role->getId()}' style='margin-top:4px; text-align:center; font-style:italic;'><a href='verein.php?ressort={$subcharge_role_username}'>{$subcharge_role_name}</a></div>";
+                $subcharge_role_id = ($has_all_permissions ? "<br/>(ID: {$subcharge_role->getId()})" : '');
+                $org .= "<div id='link-role-{$subcharge_role->getId()}' style='margin-top:4px; text-align:center; font-style:italic;'><a href='verein.php?ressort={$subcharge_role_username}'>{$subcharge_role_name}</a>{$subcharge_role_id}</div>";
                 $subcharge_role_assignees = $subcharge_role->getUsers();
                 foreach ($subcharge_role_assignees as $subcharge_role_assignee) {
                     $org .= olz_user_info_with_popup($subcharge_role_assignee, 'name');

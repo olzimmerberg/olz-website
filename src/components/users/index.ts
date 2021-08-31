f48eccdb216a45df6bedf4a748246d0447f2342f
Simon Hatt <hatt@google.com>
2021-08-31 21:20:44 +0200

    Always show user image in user info card

--- src/components/users/index.ts
+++ src/components/users/index.ts
@@ -1,4 +1,5 @@
-import './olz_popup/olz_popup.css';
-import './olz_user_info_with_popup/olz_user_info_with_popup.css';
+import './olz_popup/olz_popup.scss';
+import './olz_user_info_card/olz_user_info_card.scss';
+import './olz_user_info_with_popup/olz_user_info_with_popup.scss';
 
 export * from './olz_popup/olz_popup';
--- src/components/users/olz_user_info_card/olz_user_info_card.php
+++ src/components/users/olz_user_info_card/olz_user_info_card.php
@@ -6,10 +6,15 @@ function olz_user_info_card($user) {
     require_once __DIR__.'/../../../config/server.php';
     require_once __DIR__.'/../../../admin/olz_functions.php';
 
-    $image_path = "img/users/{$user->getId()}.jpg";
-    $out = "<table><tr><td style='width:1px;'>";
-    $out .= is_file("{$_CONFIG->getDataPath()}{$image_path}") ? "<img src='{$_CONFIG->getDataHref()}{$image_path}' alt='' style='height:64px;'>" : "&nbsp;";
-    $out .= "</td><td style='padding-left:10px;'>";
+    $image_base_path = "img/users/{$user->getId()}";
+    $img_html = "<img src='{$_CONFIG->getCodeHref()}icns/user.jpg' alt='' class='olz-user-info-card-image'>";
+    if (is_file("{$_CONFIG->getDataPath()}{$image_base_path}.jpg")) {
+        $img_html = "<img src='{$_CONFIG->getDataHref()}{$image_base_path}.jpg' alt='' class='olz-user-info-card-image'>";
+    }
+
+    $out = "<table><tr><td class='olz-user-info-card-image-td'>";
+    $out .= $img_html;
+    $out .= "</td><td class='olz-user-info-card-info-td'>";
     $out .= "<b>{$user->getFullName()}</b>";
     // $out .= ($row["adresse"] ? "<br>".$row["adresse"] : "");
     // $out .= ($row["tel"] ? "<br>Tel. ".$row["tel"] : "");

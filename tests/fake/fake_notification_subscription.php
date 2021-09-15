5df4ca2c4308d99672b7a3b2f1a2b02cba6c8c2b
Simon Hatt <hatt@google.com>
2021-09-15 16:45:08 +0000

    Fix send daily notification when telegram link has no chat ID (#566)

--- tests/fake/fake_notification_subscription.php
+++ tests/fake/fake_notification_subscription.php
@@ -2,11 +2,12 @@
 
 require_once __DIR__.'/../../src/model/NotificationSubscription.php';
 
-function get_fake_notification_subscription($delivery_type, $user, $notification_type, $notification_type_args, $created_at = null) {
+function get_fake_notification_subscription($id, $delivery_type, $user, $notification_type, $notification_type_args, $created_at = null) {
     if ($created_at == null) {
         $created_at = new DateTime('2020-03-13 19:30:00');
     }
     $notification_subscription = new NotificationSubscription();
+    $notification_subscription->setId($id);
     $notification_subscription->setDeliveryType($delivery_type);
     $notification_subscription->setUser($user);
     $notification_subscription->setNotificationType($notification_type);

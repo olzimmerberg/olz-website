7d6f791e320b0a8b8c051e3174f6f5edd527c0fe
Simon Hatt <hatt@google.com>
2021-10-19 18:31:45 +0200

    Return 400 for old ?page=... navigation

--- src/index.php
+++ src/index.php
@@ -5,6 +5,9 @@ require_once __DIR__.'/utils/env/EnvUtils.php';
 
 $env_utils = EnvUtils::fromEnv();
 $logger = $env_utils->getLogsUtils()->getLogger(basename(__FILE__));
+
 $http_utils = HttpUtils::fromEnv();
 $http_utils->setLogger($logger);
+$http_utils->validateGetParams([], $_GET);
+
 $http_utils->redirect("{$env_utils->getCodeHref()}startseite.php", 308);

78b4a4d939b9d7317cbb798f476cb27566de5eed
Simon Hatt <hatt@google.com>
2021-10-30 02:12:41 +0200

    Remove Galerie from search engines

--- src/sitemap.php
+++ src/sitemap.php
@@ -30,7 +30,6 @@ echo get_entry("{$base_url}", 'daily', '0.6');
 echo get_entry("{$base_url}aktuell.php", 'daily', '0.6');
 echo get_entry("{$base_url}blog.php", 'daily', '0.4');
 echo get_entry("{$base_url}termine.php", 'daily', '0.6');
-echo get_entry("{$base_url}galerie.php", 'daily', '0.5');
 echo get_entry("{$base_url}karten.php", 'monthly', '0.5');
 echo get_entry("{$base_url}material.php", 'monthly', '0.5');
 echo get_entry("{$base_url}service.php", 'monthly', '0.3');
@@ -41,11 +40,6 @@ echo get_entry("{$base_url}trophy.php", 'monthly', '0.5');
 $aktuell_ids = $entityManager->getRepository(NewsEntry::class)->getAllActiveIds();
 foreach ($aktuell_ids as $aktuell_id) {
     echo get_entry("{$base_url}aktuell.php?id={$aktuell_id}", 'monthly', '0.3');
-}
-
-$galerie_ids = $entityManager->getRepository(Galerie::class)->getAllActiveIds();
-foreach ($galerie_ids as $galerie_id) {
-    echo get_entry("{$base_url}galerie.php?id={$galerie_id}", 'monthly', '0.2');
 }
 
 $verein_ressorts = $entityManager->getRepository(Role::class)->getAllActiveRessorts();

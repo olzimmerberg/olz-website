34d3703a58a3aa7a17bf90653469b7bcd03a1832
Simon Hatt <hatt@google.com>
2021-11-02 23:29:08 +0000

    Re-enable endpoint logging (#593)

--- src/api/OlzEndpoint.php
+++ src/api/OlzEndpoint.php
@@ -3,9 +3,16 @@
 use PhpTypeScriptApi\Endpoint;
 
 abstract class OlzEndpoint extends Endpoint {
+    use \Psr\Log\LoggerAwareTrait;
+
     public function runtimeSetup() {
         require_once __DIR__.'/../utils/session/StandardSession.php';
+        require_once __DIR__.'/../utils/env/EnvUtils.php';
+        $env_utils = EnvUtils::fromEnv();
+        $logger_name = "Endpoint:{$this->getIdent()}";
+        $logger = $env_utils->getLogsUtils()->getLogger($logger_name);
         $session = new StandardSession();
+        $this->setLogger($logger);
         $this->setServer($_SERVER);
         $this->setSession($session);
     }

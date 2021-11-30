ce8b70a7fcf83fa207eb2e90e239070c3a2342b9
Simon Hatt <hatt@google.com>
2021-11-30 10:37:18 +0100

    Remove old newsletter (#621)

--- tests/screenshot_tests/modules/newsletter.php
+++ tests/screenshot_tests/modules/newsletter.php
@@ -13,40 +13,112 @@ function test_newsletter($driver, $base_url) {
     global $service_url, $name, $email;
     tick('newsletter');
 
+    login($driver, $base_url, 'vorstand', 'v0r57and');
     $driver->get("{$base_url}{$service_url}");
-    $subscribe_elem = $driver->findElement(
-        WebDriverBy::cssSelector('input[name="buttonnewsletter"][value="Anmelden"]')
-    );
-    $subscribe_elem->click();
-    $name_elem = $driver->findElement(
-        WebDriverBy::cssSelector('input[name="newslettername"]')
-    );
-    $name_elem->sendKeys($name);
-    $email_elem = $driver->findElement(
-        WebDriverBy::cssSelector('input[name="newsletteremail"]')
-    );
-    $email_elem->sendKeys($email);
-    $forum_elem = $driver->findElement(
-        WebDriverBy::cssSelector('input[name="newsletterkategorie[]"][value="forum"]')
-    );
-    $forum_elem->click();
-    $code_elem = $driver->findElement(
-        WebDriverBy::cssSelector('input[name="newsletteruid"]')
-    );
-    $code = $code_elem->getAttribute("value");
-    take_pageshot($driver, 'newsletter_form');
+    $driver->navigate()->refresh();
+    $driver->get("{$base_url}{$service_url}");
 
-    $preview_elem = $driver->findElement(
-        WebDriverBy::cssSelector('input[name="buttonnewsletter"][value="Vorschau"]')
-    );
-    $preview_elem->click();
-    take_pageshot($driver, 'newsletter_preview');
+    take_pageshot($driver, 'newsletter_vorstand');
 
-    $save_elem = $driver->findElement(
-        WebDriverBy::cssSelector('input[name="buttonnewsletter"][value="Speichern"]')
+    login($driver, $base_url, 'admin', 'adm1n');
+    $driver->get("{$base_url}{$service_url}");
+    $driver->navigate()->refresh();
+    $driver->get("{$base_url}{$service_url}");
+
+    take_pageshot($driver, 'newsletter_original');
+
+    $telegram_monthly_preview_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#telegram-notifications-form input[name="monthly-preview"]')
     );
-    $save_elem->click();
-    take_pageshot($driver, 'newsletter_saved');
+    $telegram_monthly_preview_elem->click();
+    $telegram_weekly_preview_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#telegram-notifications-form input[name="weekly-preview"]')
+    );
+    $telegram_weekly_preview_elem->click();
+    $telegram_deadline_warning_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#telegram-notifications-form input[name="deadline-warning"]')
+    );
+    $telegram_deadline_warning_elem->click();
+    $telegram_deadline_warning_days_elem = new WebDriverSelect($driver->findElement(
+        WebDriverBy::cssSelector('#telegram-notifications-form select[name="deadline-warning-days"]')
+    ));
+    $telegram_deadline_warning_days_elem->selectByVisibleText('2');
+    $telegram_daily_summary_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#telegram-notifications-form input[name="daily-summary"]')
+    );
+    $telegram_daily_summary_elem->click();
+    $telegram_daily_summary_aktuell_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#telegram-notifications-form input[name="daily-summary-aktuell"]')
+    );
+    $telegram_daily_summary_aktuell_elem->click();
+    $telegram_daily_summary_blog_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#telegram-notifications-form input[name="daily-summary-blog"]')
+    );
+    $telegram_daily_summary_blog_elem->click();
+    $telegram_weekly_summary_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#telegram-notifications-form input[name="weekly-summary"]')
+    );
+    $telegram_weekly_summary_elem->click();
+    $telegram_weekly_summary_forum_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#telegram-notifications-form input[name="weekly-summary-forum"]')
+    );
+    $telegram_weekly_summary_forum_elem->click();
+    $telegram_weekly_summary_galerie_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#telegram-notifications-form input[name="weekly-summary-galerie"]')
+    );
+    $telegram_weekly_summary_galerie_elem->click();
+    $telegram_submit_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#telegram-notifications-submit')
+    );
+    $telegram_submit_elem->click();
+
+    $email_monthly_preview_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#email-notifications-form input[name="monthly-preview"]')
+    );
+    $email_monthly_preview_elem->click();
+    $email_weekly_preview_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#email-notifications-form input[name="weekly-preview"]')
+    );
+    $email_weekly_preview_elem->click();
+    $email_deadline_warning_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#email-notifications-form input[name="deadline-warning"]')
+    );
+    $email_deadline_warning_elem->click();
+    $email_deadline_warning_days_elem = new WebDriverSelect($driver->findElement(
+        WebDriverBy::cssSelector('#email-notifications-form select[name="deadline-warning-days"]')
+    ));
+    $email_deadline_warning_days_elem->selectByVisibleText('2');
+    $email_daily_summary_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#email-notifications-form input[name="daily-summary"]')
+    );
+    $email_daily_summary_elem->click();
+    $email_daily_summary_aktuell_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#email-notifications-form input[name="daily-summary-aktuell"]')
+    );
+    $email_daily_summary_aktuell_elem->click();
+    $email_daily_summary_blog_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#email-notifications-form input[name="daily-summary-blog"]')
+    );
+    $email_daily_summary_blog_elem->click();
+    $email_weekly_summary_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#email-notifications-form input[name="weekly-summary"]')
+    );
+    $email_weekly_summary_elem->click();
+    $email_weekly_summary_forum_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#email-notifications-form input[name="weekly-summary-forum"]')
+    );
+    $email_weekly_summary_forum_elem->click();
+    $email_weekly_summary_galerie_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#email-notifications-form input[name="weekly-summary-galerie"]')
+    );
+    $email_weekly_summary_galerie_elem->click();
+    $email_submit_elem = $driver->findElement(
+        WebDriverBy::cssSelector('#email-notifications-submit')
+    );
+    $email_submit_elem->click();
+    take_pageshot($driver, 'newsletter_modified');
+
+    logout($driver, $base_url);
 
     reset_dev_data();
     tock('newsletter', 'newsletter');

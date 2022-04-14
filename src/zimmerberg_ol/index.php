47d3ecd62f3f1df3d970e276c149ac21dfa4e4c1
Simon Hatt <hatt@google.com>
2022-04-14 14:33:07 +0200

    ZOL: Logo & colors (#706)

--- src/zimmerberg_ol/index.php
+++ src/zimmerberg_ol/index.php
@@ -12,7 +12,7 @@ echo <<<ZZZZZZZZZZ
 <head>
 	<title>{$event_title}</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
+	<link rel="icon" href="./favicon.ico" type="image/vnd.microsoft.icon" />
 	<link rel="stylesheet" href="styles.css"> 
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -30,7 +30,6 @@ echo <<<ZZZZZZZZZZ
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		  <span class="navbar-toggler-icon"></span>
 		</button>
-	  
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item dropdown">
@@ -75,6 +74,9 @@ echo <<<ZZZZZZZZZZ
 				</li>
 				-->
 			</ul>
+		</div>
+		<div class="logo-container">
+			<img src="{$img_root}logo_100.png" alt="" class="logo-img">
 		</div>
 	</nav>
 

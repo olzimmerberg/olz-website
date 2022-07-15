c8603b6c10be9d590f9bb508e67cc00c6476f87d
Simon Hatt <hatt@google.com>
2022-07-15 16:03:06 +0200

    Make App list UI smaller

--- src/Controller/AppsController.php
+++ src/Controller/AppsController.php
@@ -27,22 +27,22 @@ class AppsController extends AbstractController {
         $out .= "<div id='content_double'>";
         $out .= "<h1>Apps</h1>";
         $out .= "<div style='display:flex; flex-wrap:wrap;'>";
-        $out .= implode('<br>', array_map(function ($app) {
+        $out .= implode('', array_map(function ($app) {
             $icon = $app->getIcon();
             $display_name = $app->getDisplayName();
             $href = $app->getHref();
             $basename = $app->getBasename();
             return <<<ZZZZZZZZZZ
             <a href='{$href}'>
-                <div style='width:250px;text-align:center;'>
-                    <img src='{$icon}' alt='{$basename}-icon' class='noborder' style='max-width:100px;' />
+                <div style='width:160px;text-align:center;'>
+                    <img src='{$icon}' alt='{$basename}-icon' class='noborder' style='max-width:80px;' />
                     <div>{$display_name}</div>
                 </div>
             </a>
             ZZZZZZZZZZ;
         }, OlzApps::getAppsForUser($user)));
         $out .= "</div>";
-        $out .= "<div style='margin-top:100px;'>Einige Icons mit Emojis von <a href='https://openmoji.org/' target='_blank'>OpenMoji</a> &mdash; dem open-source Emoji- und Icon-Projekt. Lizenz: <a href='https://creativecommons.org/licenses/by-sa/4.0/#' target='_blank'>CC BY-SA 4.0</a></div>";
+        $out .= "<div style='margin-top:80px;'>Einige Icons mit Emojis von <a href='https://openmoji.org/' target='_blank'>OpenMoji</a> &mdash; dem open-source Emoji- und Icon-Projekt. Lizenz: <a href='https://creativecommons.org/licenses/by-sa/4.0/#' target='_blank'>CC BY-SA 4.0</a></div>";
         $out .= "</div>";
         $out .= OlzFooter::render([]);
 

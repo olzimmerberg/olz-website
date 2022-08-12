e651a5e1ec2890781891b3589797d1c67bd2f8c5
Simon Hatt <hatt@google.com>
2022-08-12 17:56:25 +0200

    Fix Files permissions, quiz access, results index (#784)

--- src/Apps/Results/Components/OlzResults/OlzResults.php
+++ src/Apps/Results/Components/OlzResults/OlzResults.php
@@ -5,6 +5,7 @@ namespace Olz\Apps\Results\Components\OlzResults;
 use Olz\Apps\Results\Metadata;
 use Olz\Components\Page\OlzFooter\OlzFooter;
 use Olz\Components\Page\OlzHeader\OlzHeader;
+use Olz\Utils\EnvUtils;
 
 class OlzResults {
     public static function render() {
@@ -15,14 +16,29 @@ class OlzResults {
             'norobots' => true,
         ]);
 
-        $out .= "<div id='content_double' style='position:relative'>";
-        $out .= <<<'ZZZZZZZZZZ'
-        <div id='title-box'><div id='backbutton' onclick='olz.popHash()'>&lt;</div><h1 id='title'></h1></div>
-        <div id='classes-box'></div>
-        <div id='content-box'></div>
-        <div class='inactive' id='grafik-box'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' style='width:100%; height:100%;' id='grafik-svg'></svg></div>
-        ZZZZZZZZZZ;
-        $out .= "</div>";
+        if (isset($_GET['file'])) {
+            $out .= "<div id='content_double' style='position:relative'>";
+            $out .= <<<'ZZZZZZZZZZ'
+            <div id='title-box'><div id='backbutton' onclick='olz.popHash()'>&lt;</div><h1 id='title'></h1></div>
+            <div id='classes-box'></div>
+            <div id='content-box'></div>
+            <div class='inactive' id='grafik-box'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' style='width:100%; height:100%;' id='grafik-svg'></svg></div>
+            ZZZZZZZZZZ;
+            $out .= "</div>";
+        } else {
+            $out .= "<div id='content_double'>";
+            $out .= "<ul>";
+            $env_utils = EnvUtils::fromEnv();
+            $data_path = $env_utils->getDataPath();
+            $contents = scandir("{$data_path}results");
+            foreach ($contents as $entry) {
+                if (preg_match('/\.xml$/', $entry) && !preg_match('/\.bak\./', $entry)) {
+                    $out .= "<li><a href='?file={$entry}'>{$entry}</a></li>\n";
+                }
+            }
+            $out .= "</ul>";
+            $out .= "</div>";
+        }
 
         $metadata = new Metadata();
         $out .= $metadata->getJsCssImports();

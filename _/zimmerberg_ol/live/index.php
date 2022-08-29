19a743992c2237e7fc02f3574cb9b0c0199ea760
Simon Hatt <hatt@google.com>
2022-08-29 14:42:24 +0200

    Zimmerberg OL Banner (#818)

--- _/zimmerberg_ol/live/index.php
+++ _/zimmerberg_ol/live/index.php
@@ -4,11 +4,11 @@ $today = date('Y-m-d');
 $link_saturday = 'http://www.compass-zos.ch/resultate/resultate_13_nat_ol_2021.html';
 $link_sunday = 'https://liveresultat.orientering.se/followfull.php?comp=20113&lang=de';
 
-if ($today === '2021-10-02') {
+if ($today === '2022-10-01') {
     header("Location: {$link_saturday}");
     exit('Umleitung...');
 }
-if ($today === '2021-10-03') {
+if ($today === '2022-10-02') {
     header("Location: {$link_sunday}");
     exit('Umleitung...');
 }

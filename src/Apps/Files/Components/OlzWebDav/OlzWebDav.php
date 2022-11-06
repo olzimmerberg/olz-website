5aee3b20162fc73b9c92aff930c3892026d28e87
Simon Hatt <hatt@google.com>
2022-11-06 16:28:24 +0100

    Fix WebDAV (#904)

--- src/Apps/Files/Components/OlzWebDav/OlzWebDav.php
+++ src/Apps/Files/Components/OlzWebDav/OlzWebDav.php
@@ -8,19 +8,19 @@ use Olz\Utils\HttpUtils;
 use Sabre\DAV;
 
 class OlzWebDav {
-    public static function render() {
+    public static function render($args = []) {
         require_once __DIR__.'/../../../../../_/config/paths.php';
 
         error_reporting(0);
 
         // Hack: Use weird path-token-authentication
         // Reason: Hoststar cannot have basic auth header ¯\_(ツ)_/¯
-        $path_info = $_SERVER['PATH_INFO'];
-        $pattern = '/^(\/?access_token__([a-zA-Z0-9_\-]+))(\/.*)?$/';
+        $path_info = $args['path'];
+        $pattern = '/^\/?(token__([a-zA-Z0-9_\-]+))(\/.*)?$/';
         $res = preg_match($pattern, $path_info, $matches);
         $stripped_path_info = $res ? $matches[1] : '';
         $access_token = $res ? $matches[2] : null;
-        $simulated_path_info = $res ? $matches[3] : $path_info;
+        $simulated_path_info = $res ? $matches[3] || '/' : $path_info;
         $_SERVER['PATH_INFO'] = $simulated_path_info;
         // end of hack
 
@@ -38,7 +38,7 @@ class OlzWebDav {
 
         $root_directory = new DAV\FS\Directory("{$data_path}OLZimmerbergAblage/{$user_root}");
         $server = new DAV\Server($root_directory);
-        $server->setBaseUri("/apps/files/webdav{$stripped_path_info}");
+        $server->setBaseUri("/apps/files/webdav/{$stripped_path_info}");
 
         $auth_backend = new CallbackAuthBackend(
             function () use ($auth_utils, $user) {

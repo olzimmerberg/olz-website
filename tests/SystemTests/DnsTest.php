ec9af4f2893d0a8bf00e1384acaf59eb1b215dc9
Simon Hatt <hatt@google.com>
2022-11-21 12:15:50 +0100

    Fix ProcessEmailTask for emails without message ID, add DNS system test

---
+++ tests/SystemTests/DnsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Olz\Tests\SystemTests;
+
+use Olz\Tests\SystemTests\Common\SystemTestCase;
+
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+final class DnsTest extends SystemTestCase {
+    public static $dnsHostname = "olzimmerberg.ch";
+
+    public function testHasGoogleSiteVerificationRecord(): void {
+        $records = dns_get_record("{$this::$dnsHostname}", DNS_TXT);
+        $has_google_site_verification = false;
+        foreach ($records as $record) {
+            if (preg_match('/^google\-site\-verification\=/i', $record['txt'])) {
+                $has_google_site_verification = true;
+            }
+        }
+        $this->assertTrue($has_google_site_verification, json_encode($records));
+    }
+}

3fdc0f0fece59d2cfa9e6e6322dc9154c7c21574
Simon Hatt <hatt@google.com>
2023-02-22 21:00:55 +0100

    Replace session_start with session_start_if_cookie_set fixup (#1002)

--- _/konto_facebook.php
+++ _/konto_facebook.php
@@ -7,7 +7,7 @@ use Olz\Utils\FacebookUtils;
 
 require_once __DIR__.'/config/init.php';
 
-session_start();
+session_start_if_cookie_set();
 
 require_once __DIR__.'/admin/olz_functions.php';
 echo OlzHeader::render([

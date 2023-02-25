d62fc220c11953a488f3e7c2038a70b58629112d
Simon Hatt <hatt@google.com>
2023-02-25 12:09:50 +0100

    Attempt at fixing session lifetime

--- _/config/init.php
+++ _/config/init.php
@@ -1,16 +1,17 @@
 <?php
 
-// Ampersand-Ausgabe
+// Ampersand output
 ini_set('arg_separator.output', '&amp;');
 
-// Sprache für Datum-/Zeitangaben setzen
+// Language for Date / Time output
 setlocale(LC_ALL, 'de_DE@euro', 'de_DE', 'de_DE.UTF8');
 
 // Character encoding
 mb_internal_encoding('UTF-8');
 
-// Session-Sicherheit
+// Session security
 if (!headers_sent()) {
+    ini_set('session.gc_maxlifetime', 2419200); // keep one month
     ini_set('session.cookie_httponly', 1);
     $server_name = $_SERVER['SERVER_NAME'] ?? '';
     if ($server_name != '127.0.0.1' && $server_name != 'localhost') {

e25a4610439c5ce5bf974531c1836e0b87ff110e
Simon Hatt <hatt@google.com>
2023-04-08 15:09:20 +0200

    Enable legacy file references again (termine, downloads) (#1074)

--- tests/UnitTests/Utils/FileUtilsTest.php
+++ tests/UnitTests/Utils/FileUtilsTest.php
@@ -14,7 +14,20 @@ use Olz\Utils\FileUtils;
  * @covers \Olz\Utils\FileUtils
  */
 final class FileUtilsTest extends UnitTestCase {
-    public function testOlzFile(): void {
+    public function testOlzFileNotMigrated(): void {
+        $file_utils = new FileUtils();
+        $env_utils = new Fake\FakeEnvUtils();
+        $file_utils->setEnvUtils($env_utils);
+        $data_path = $env_utils->getDataPath();
+        $sample_file_path = __DIR__.'/../../../src/Utils/data/sample-data/sample-document.pdf';
+        $file_path = "{$data_path}files/aktuell/123/001.pdf";
+        mkdir(dirname($file_path), 0777, true);
+        copy($sample_file_path, $file_path);
+        touch($file_path, strtotime('2020-03-13 19:30:00'));
+        $this->assertSame("<a href='/data-href/files/aktuell//123/001.pdf?modified=1584127800' style='padding-left:19px; background-image:url(/_/file_tools.php?request=thumb&db_table=aktuell&id=123&index=1&dim=16); background-repeat:no-repeat;'>Test</a>", $file_utils->olzFile('aktuell', 123, 1, "Test"));
+    }
+
+    public function testOlzFileMigrated(): void {
         $file_utils = new FileUtils();
         $env_utils = new Fake\FakeEnvUtils();
         $file_utils->setEnvUtils($env_utils);
@@ -27,7 +40,20 @@ final class FileUtilsTest extends UnitTestCase {
         $this->assertSame("<a href='/data-href/files/news//123/abcdefghijklmnopqrstuvwx.pdf?modified=1584127800' style='padding-left:19px; background-image:url(/_/file_tools.php?request=thumb&db_table=news&id=123&index=abcdefghijklmnopqrstuvwx.pdf&dim=16); background-repeat:no-repeat;'>Test</a>", $file_utils->olzFile('news', 123, 'abcdefghijklmnopqrstuvwx.pdf', "Test"));
     }
 
-    public function testReplaceFileTags(): void {
+    public function testReplaceFileTagsNotMigrated(): void {
+        $file_utils = new FileUtils();
+        $env_utils = new Fake\FakeEnvUtils();
+        $file_utils->setEnvUtils($env_utils);
+        $data_path = $env_utils->getDataPath();
+        $sample_file_path = __DIR__.'/../../../src/Utils/data/sample-data/sample-document.pdf';
+        $file_path = "{$data_path}files/aktuell/123/001.pdf";
+        mkdir(dirname($file_path), 0777, true);
+        copy($sample_file_path, $file_path);
+        touch($file_path, strtotime('2020-03-13 19:30:00'));
+        $this->assertSame("test <a href='/data-href/files/aktuell//123/001.pdf?modified=1584127800' style='padding-left:19px; background-image:url(/_/file_tools.php?request=thumb&db_table=aktuell&id=123&index=1&dim=16); background-repeat:no-repeat;'>Datei</a> text", $file_utils->replaceFileTags('test <DATEI1 text="Datei"> text', 'aktuell', 123));
+    }
+
+    public function testReplaceFileTagsMigrated(): void {
         $file_utils = new FileUtils();
         $env_utils = new Fake\FakeEnvUtils();
         $file_utils->setEnvUtils($env_utils);

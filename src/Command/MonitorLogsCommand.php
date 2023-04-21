c4494a7d7c1f5dc62f6e88ffcdb71d541e580d97
Simon Hatt <hatt@google.com>
2023-04-21 11:27:57 +0200

    Update MonitorLogsCommand.php

--- src/Command/MonitorLogsCommand.php
+++ src/Command/MonitorLogsCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(name: 'olz:get-id-algos')]
+#[AsCommand(name: 'olz:monitor-logs')]
 class MonitorLogsCommand extends OlzCommand {
     protected function getAllowedAppEnvs(): array {
         return ['dev', 'test', 'staging', 'prod'];

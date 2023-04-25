e5637bd62b126e5a5523316a6ba1b8f290b9ce37
Simon Hatt <hatt@google.com>
2023-04-25 19:26:23 +0200

    Infinite scrolling for logs app (#1115)

--- src/Apps/Logs/Components/OlzLogs/OlzLogs.tsx
+++ src/Apps/Logs/Components/OlzLogs/OlzLogs.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import $ from 'jquery';
 
 import {olzApi} from '../../../../Api/client';
 import {OlzLogLevel, OlzLogsQuery} from '../../../../Api/client/generated_olz_api_types';
@@ -204,93 +203,6 @@ export const OlzLogs = (): React.ReactElement => {
         </div>
     </>);
 };
-
-// let logIndex = 0;
-
-// export function olzLogsGetFirstLog(): boolean {
-//     logIndex = 0;
-//     return olzLogsGetNextLog();
-// }
-
-// export function olzLogsGetNextLog(): boolean {
-//     olzApi.call(
-//         'getLogs',
-//         {index: logIndex},
-//     )
-//         .then((response) => {
-//             logIndex++;
-//             $('#logs').prepend(processLogs(response.content ?? ''));
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-//     return false;
-// }
-
-// export function olzLogsFetch(): void {
-//     const dateFilterInput = document.getElementById('date-filter-input') as HTMLInputElement;
-//     const logLevelFilterSelect = document.getElementById('log-level-filter-select') as HTMLSelectElement;
-//     const textSearchFilterInput = document.getElementById('text-search-filter-input') as HTMLInputElement;
-
-//     const query = getQuery(
-//         dateFilterInput.value,
-//         logLevelFilterSelect.value,
-//         textSearchFilterInput.value,
-//     );
-
-//     olzApi.call(
-//         'getLogs',
-//         {query},
-//     ).then((response) => {
-//         const logsElem = document.getElementById('logs');
-//         if (!logsElem) {
-//             return;
-//         }
-//         logsElem.innerHTML = processLogs(response.content ?? '');
-//     });
-// }
-
-export function olzLogsLevelFilterChange(): void {
-    const logLevelFilterSelect = document.getElementById('log-level-filter-select') as HTMLSelectElement;
-    const newLogLevelFilter = logLevelFilterSelect.value;
-    $('#logs').removeClass('levels-all');
-    $('#logs').removeClass('levels-info-higher');
-    $('#logs').removeClass('levels-notice-higher');
-    $('#logs').removeClass('levels-warning-higher');
-    $('#logs').removeClass('levels-error-higher');
-    $('#logs').addClass(newLogLevelFilter);
-}
-
-export function processLogs(lines: string[]): string {
-    const formattingRegex = /(\S+)\.(DEBUG|INFO|NOTICE|WARNING|ERROR|CRITICAL|ALERT|EMERGENCY)/;
-    return lines
-        .map((line) => line
-            .replace(/&/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;'))
-        .map((line) => (
-            shouldLineBeGreyedOut(line)
-                ? `<div class='greyed-out'>${line}</div>`
-                : line
-        ))
-        .map((line) => {
-            let logLevel = 'unknown';
-            const replacedLine = line.replace(
-                formattingRegex,
-                (match) => {
-                    const res = formattingRegex.exec(match);
-                    if (!res) {
-                        return match;
-                    }
-                    logLevel = res[2].toLowerCase();
-                    return `<span class='log-channel'>${res[1]}</span>.<span class='log-level'>${res[2]}</span>`;
-                },
-            );
-            return `<div class='log-line level-${logLevel}'>${replacedLine}</div>`;
-        })
-        .join('\n');
-}
 
 export function shouldLineBeGreyedOut(line: string): boolean {
     const monitorCommandRegex = /\s+Olz\\Command\\Monitor/;

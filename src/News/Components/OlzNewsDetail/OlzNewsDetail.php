91d42b2a2a5154f81b2da4a97312215bfdbe4e60
Simon Hatt <hatt@google.com>
2023-06-25 14:05:44 +0200

    Fix news detail, remove unused code (#1173)

--- src/News/Components/OlzNewsDetail/OlzNewsDetail.php
+++ src/News/Components/OlzNewsDetail/OlzNewsDetail.php
@@ -22,7 +22,7 @@ use PhpTypeScriptApi\Fields\FieldTypes;
 
 class OlzNewsDetail extends OlzComponent {
     public function getHtml($args = []): string {
-        global $_GET, $_POST, $_SESSION, $_SERVER;
+        global $_GET, $_POST, $_SERVER;
 
         require_once __DIR__.'/../../../../_/config/init.php';
         require_once __DIR__.'/../../../../_/config/date.php';
@@ -72,16 +72,6 @@ class OlzNewsDetail extends OlzComponent {
 
         $out = '';
 
-        $button_name = 'button'.$db_table;
-        if (isset($_GET[$button_name])) {
-            $_POST[$button_name] = $_GET[$button_name];
-        }
-        if (isset($_POST[$button_name])) {
-            $_SESSION['edit']['db_table'] = $db_table;
-        }
-
-        $zugriff = ((($_SESSION['auth'] ?? null) == 'all') or in_array($db_table, preg_split('/ /', $_SESSION['auth'] ?? ''))) ? '1' : '0';
-
         $sql = "SELECT * FROM {$db_table} WHERE (id = '{$id}') ORDER BY datum DESC";
         $result = $db->query($sql);
         $row = $result->fetch_assoc();
@@ -99,7 +89,6 @@ class OlzNewsDetail extends OlzComponent {
             ],
         ]);
 
-        $id_edit = $_SESSION['id_edit'] ?? ''; // TODO: Entfernen?
         $pretty_date = $this->dateUtils()->olzDate("tt.mm.jjjj", $row['datum']);
         $author_user = $row['author_user_id'] ?
             $user_repo->findOneBy(['id' => $row['author_user_id']]) : null;
@@ -115,58 +104,18 @@ class OlzNewsDetail extends OlzComponent {
         ]);
 
         $out .= <<<ZZZZZZZZZZ
-        <div class='content-right optional'>
+        <div class='content-right'>
             <div style='padding:4px 3px 10px 3px;'>
                 <b>Datum: </b>{$pretty_date}<br />
                 <b>Autor: </b>{$pretty_author}
             </div>
         </div>
         <div class='content-middle'>
-        <form name='Formularl' method='post' action='news/{$id}#id_edit{$id_edit}' enctype='multipart/form-data'>
         ZZZZZZZZZZ;
 
-        // -------------------------------------------------------------
-        // DATENSATZ EDITIEREN
-        if ($zugriff) {
-            $functions = ['neu' => 'Neuer Eintrag',
-                'edit' => 'Bearbeiten',
-                'abbruch' => 'Abbrechen',
-                'vorschau' => 'Vorschau',
-                'save' => 'Speichern',
-                'delete' => 'Löschen',
-                'start' => 'start',
-                'upload' => 'Upload',
-                'deletebild1' => 'BILD 1 entfernen',
-                'deletebild2' => 'BILD 2 entfernen',
-                'deletebild3' => 'BILD 3 entfernen',
-                'undo' => 'undo', ];
-        } else {
-            $functions = [];
-        }
-        $function = array_search($_POST[$button_name] ?? null, $functions);
-        if ($function != "") {
-            ob_start();
-            include __DIR__.'/../../../../_/admin/admin_db.php';
-            $out .= ob_get_contents();
-            ob_end_clean();
-        }
-        if (($_SESSION['edit']['table'] ?? null) == $db_table) {
-            $db_edit = "1";
-        } else {
-            $db_edit = "0";
-        }
+        $out .= OlzNewsArticle::render(['id' => $id]);
 
-        // -------------------------------------------------------------
-        // AKTUELL - VORSCHAU
-        if (($db_edit == "0") or (($do ?? null) == 'vorschau')) {
-            $out .= OlzNewsArticle::render([
-                'id' => $id,
-                'is_preview' => (($do ?? null) == 'vorschau'),
-            ]);
-        }
-
-        $out .= "</form>
-        </div>";
+        $out .= "</div>";
 
         $out .= OlzFooter::render();
 

d4aee6d1a86aa3e50d198773f8296731a04bb75a
Simon Hatt <hatt@google.com>
2023-08-11 15:00:12 +0200

    Remove unused migration configs (#1205)

--- src/Utils/data/db_structure.sql
+++ src/Utils/data/db_structure.sql
@@ -418,7 +418,7 @@ CREATE TABLE `panini24` (
   CONSTRAINT `FK_1254A2E52B18554A` FOREIGN KEY (`owner_user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `FK_1254A2E55A75A473` FOREIGN KEY (`owner_role_id`) REFERENCES `roles` (`id`),
   CONSTRAINT `FK_1254A2E57D182D95` FOREIGN KEY (`created_by_user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1003 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;

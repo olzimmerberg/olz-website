b5dd4788825f13f477d3f2ed135169a5107ae3be
Simon Hatt <hatt@google.com>
2023-08-12 11:24:27 +0200

    Fix editNews, test with empty case (#1206)

--- src/News/Endpoints/GetNewsEndpoint.php
+++ src/News/Endpoints/GetNewsEndpoint.php
@@ -31,6 +31,7 @@ class GetNewsEndpoint extends OlzGetEntityEndpoint {
         $author_name = $news_entry->getAuthorName();
         $author_email = $news_entry->getAuthorEmail();
         $tags_for_api = $this->getTagsForApi($news_entry->getTags() ?? '');
+        $external_url = $news_entry->getExternalUrl();
         $termin_id = $news_entry->getTermin();
 
         $file_ids = [];
@@ -59,7 +60,7 @@ class GetNewsEndpoint extends OlzGetEntityEndpoint {
                 'title' => $news_entry->getTitle(),
                 'teaser' => $news_entry->getTeaser(),
                 'content' => $news_entry->getContent(),
-                'externalUrl' => $news_entry->getExternalUrl(),
+                'externalUrl' => $external_url ? $external_url : null,
                 'tags' => $tags_for_api,
                 'terminId' => $termin_id ? $termin_id : null,
                 'imageIds' => $news_entry->getImageIds(),
--- tests/UnitTests/News/Endpoints/EditNewsEndpointTest.php
+++ tests/UnitTests/News/Endpoints/EditNewsEndpointTest.php
@@ -12,6 +12,7 @@ use PhpTypeScriptApi\HttpError;
 
 class FakeEditNewsEndpointNewsRepository {
     public function findOneBy($where) {
+        // Minimal
         if ($where === ['id' => 12]) {
             $entry = new NewsEntry();
             $entry->setId(12);
@@ -21,9 +22,27 @@ class FakeEditNewsEndpointNewsRepository {
             $entry->setContent("");
             return $entry;
         }
+        // Empty
         if ($where === ['id' => 123]) {
             $entry = new NewsEntry();
             $entry->setId(123);
+            $entry->setDate('0000-01-01');
+            $entry->setTime('00:00:00');
+            $entry->setFormat('aktuell');
+            $entry->setTitle("Cannot be empty");
+            $entry->setTeaser("");
+            $entry->setContent("");
+            $entry->setTags('');
+            $entry->setTermin('');
+            $entry->setExternalUrl('');
+            return $entry;
+        }
+        // Maximal
+        if ($where === ['id' => 1234]) {
+            $entry = new NewsEntry();
+            $entry->setId(1234);
+            $entry->setDate('2020-03-13');
+            $entry->setTime('18:00:00');
             $entry->setFormat('aktuell');
             $entry->setTitle("Fake title");
             $entry->setTeaser("Fake teaser");
@@ -157,6 +176,48 @@ final class EditNewsEndpointTest extends UnitTestCase {
         ], $result);
     }
 
+    public function testEditNewsEndpointEmpty(): void {
+        WithUtilsCache::get('authUtils')->has_permission_by_query = ['any' => true];
+        $entity_manager = WithUtilsCache::get('entityManager');
+        $news_repo = new FakeEditNewsEndpointNewsRepository();
+        $entity_manager->repositories[NewsEntry::class] = $news_repo;
+        WithUtilsCache::get('entityUtils')->can_update_olz_entity = true;
+        $endpoint = new EditNewsEndpoint();
+        $endpoint->runtimeSetup();
+
+        $result = $endpoint->call([
+            'id' => 123,
+        ]);
+
+        $this->assertSame([
+            "INFO Valid user request",
+            "INFO Valid user response",
+        ], $this->getLogs());
+        $this->assertSame([
+            'id' => 123,
+            'meta' => [
+                'ownerUserId' => null,
+                'ownerRoleId' => null,
+                'onOff' => false,
+            ],
+            'data' => [
+                'format' => 'aktuell',
+                'authorUserId' => null,
+                'authorRoleId' => null,
+                'authorName' => null,
+                'authorEmail' => null,
+                'title' => 'Cannot be empty',
+                'teaser' => '',
+                'content' => '',
+                'externalUrl' => null,
+                'tags' => [],
+                'terminId' => null,
+                'imageIds' => null,
+                'fileIds' => [],
+            ],
+        ], $result);
+    }
+
     public function testEditNewsEndpointMaximal(): void {
         WithUtilsCache::get('authUtils')->has_permission_by_query = ['any' => true];
         $entity_manager = WithUtilsCache::get('entityManager');
@@ -169,18 +230,18 @@ final class EditNewsEndpointTest extends UnitTestCase {
         mkdir(__DIR__.'/../../tmp/temp/');
         mkdir(__DIR__.'/../../tmp/img/');
         mkdir(__DIR__.'/../../tmp/img/news/');
-        mkdir(__DIR__.'/../../tmp/img/news/123/');
-        mkdir(__DIR__.'/../../tmp/img/news/123/img/');
-        file_put_contents(__DIR__.'/../../tmp/img/news/123/img/pictureA.jpg', '');
-        file_put_contents(__DIR__.'/../../tmp/img/news/123/img/pictureB.jpg', '');
+        mkdir(__DIR__.'/../../tmp/img/news/1234/');
+        mkdir(__DIR__.'/../../tmp/img/news/1234/img/');
+        file_put_contents(__DIR__.'/../../tmp/img/news/1234/img/pictureA.jpg', '');
+        file_put_contents(__DIR__.'/../../tmp/img/news/1234/img/pictureB.jpg', '');
         mkdir(__DIR__.'/../../tmp/files/');
         mkdir(__DIR__.'/../../tmp/files/news/');
-        mkdir(__DIR__.'/../../tmp/files/news/123/');
-        file_put_contents(__DIR__.'/../../tmp/files/news/123/file1.pdf', '');
-        file_put_contents(__DIR__.'/../../tmp/files/news/123/file2.pdf', '');
+        mkdir(__DIR__.'/../../tmp/files/news/1234/');
+        file_put_contents(__DIR__.'/../../tmp/files/news/1234/file1.pdf', '');
+        file_put_contents(__DIR__.'/../../tmp/files/news/1234/file2.pdf', '');
 
         $result = $endpoint->call([
-            'id' => 123,
+            'id' => 1234,
         ]);
 
         $this->assertSame([
@@ -188,7 +249,7 @@ final class EditNewsEndpointTest extends UnitTestCase {
             "INFO Valid user response",
         ], $this->getLogs());
         $this->assertSame([
-            'id' => 123,
+            'id' => 1234,
             'meta' => [
                 'ownerUserId' => null,
                 'ownerRoleId' => null,

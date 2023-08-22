cd44a5aebcb40948992ac7aa1e4ab5892e423719
Simon Hatt <hatt@google.com>
2023-08-22 18:01:11 +0200

    Remove phpmailer dependency

--- tests/Fake/DeterministicEmailUtils.php
+++ tests/Fake/DeterministicEmailUtils.php
@@ -7,15 +7,9 @@ namespace Olz\Tests\Fake;
 use Olz\Utils\EmailUtils;
 
 class DeterministicEmailUtils extends EmailUtils {
-    public $fake_olz_mailer;
-
     public function __construct() {
         $this->setEnvUtils(new FakeEnvUtils());
         $this->setGeneralUtils(new DeterministicGeneralUtils());
-    }
-
-    public function createEmail() {
-        return $this->fake_olz_mailer;
     }
 
     public function renderMarkdown($markdown) {

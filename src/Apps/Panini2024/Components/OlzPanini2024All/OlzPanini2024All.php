bc0bb642fd04547f5a2bfe583979c793e2beb41d
Simon Hatt <hatt@google.com>
2023-09-04 17:58:30 +0200

    Panini Tabelle

--- src/Apps/Panini2024/Components/OlzPanini2024All/OlzPanini2024All.php
+++ src/Apps/Panini2024/Components/OlzPanini2024All/OlzPanini2024All.php
@@ -7,6 +7,7 @@ use Olz\Components\Apps\OlzNoAppAccess\OlzNoAppAccess;
 use Olz\Components\Common\OlzComponent;
 use Olz\Components\Page\OlzFooter\OlzFooter;
 use Olz\Components\Page\OlzHeader\OlzHeader;
+use Olz\Components\Users\OlzUserInfoWithPopup\OlzUserInfoWithPopup;
 use Olz\Entity\Panini2024\Panini2024Picture;
 use Olz\Utils\HttpUtils;
 
@@ -36,29 +37,68 @@ class OlzPanini2024All extends OlzComponent {
         ]);
 
         $out .= "<div class='content-full'>";
+        $out .= "<div class='olz-panini-2024-all'>";
 
         if ($this->authUtils()->hasPermission('panini2024')) {
             $panini_repo = $entity_manager->getRepository(Panini2024Picture::class);
             $pictures = $panini_repo->findAll();
+            $out .= "<table>";
+            $out .= <<<'ZZZZZZZZZZ'
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>OLZ-Konto</th>
+                <th>Wappen</th>
+                <th>Infos</th>
+                <th>Aktiv</th>
+                <th>Bild</th>
+            </tr>
+            ZZZZZZZZZZ;
             foreach ($pictures as $picture) {
                 $id = $picture->getId();
+                $line1 = $picture->getLine1();
+                $line2 = $picture->getLine2();
+                $owner_user = $picture->getOwnerUser();
+                $user_html = OlzUserInfoWithPopup::render([
+                    'user' => $owner_user,
+                    'mode' => 'name',
+                ]);
+                $association = $picture->getAssociation();
+                $infos = $picture->getInfos();
+                $infos_emojis = '';
+                for ($i = 0; $i < 5; $i++) {
+                    if ($i !== 0) {
+                        $infos_emojis .= ' ';
+                    }
+                    $is_valid = ($infos[$i] ?? false) && strlen($infos[$i]) > 0;
+                    $infos_emojis .= $is_valid ? '✅' : '❌';
+                }
+                $on_off = $picture->getOnOff();
+                $on_off_emoji = $on_off ? '✅' : '❌';
                 $out .= <<<ZZZZZZZZZZ
-                <div>
-                    {$id}: 
-                    <img
-                        src='/apps/panini24/single/{$id}.jpg'
-                        alt='{$id}'
-                        style='max-width: 250px; max-height: 250px;'
-                    />
-                </div>
+                <tr>
+                    <td>{$id}</td>
+                    <td>{$line1}<br/>{$line2}</td>
+                    <td>{$user_html}</td>
+                    <td>{$association}</td>
+                    <td>{$infos_emojis}</td>
+                    <td>{$on_off_emoji}</td>
+                    <td id='panini-picture-{$id}'>
+                        <button onclick='olzPanini2024.showPaniniPicture(&quot;{$id}&quot;)'>
+                            anzeigen
+                        </button>
+                    </td>
+                </tr>
                 ZZZZZZZZZZ;
             }
+            $out .= "</table>";
         } else {
             $out .= OlzNoAppAccess::render([
                 'app' => $metadata,
             ]);
         }
 
+        $out .= "</div>";
         $out .= "</div>";
 
         $out .= $metadata->getJsCssImports();

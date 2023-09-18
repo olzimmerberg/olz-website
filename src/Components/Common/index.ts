8a1f77c4484d03c6e61a5fadfe96ec385c044651
Simon Hatt <hatt@google.com>
2023-09-18 16:55:20 +0200

    Also use openlayers for standard maps

--- src/Components/Common/index.ts
+++ src/Components/Common/index.ts
@@ -1,10 +1,10 @@
 import './OlzAuthorBadge/OlzAuthorBadge.scss';
 import './OlzDefaultForm/OlzDefaultForm.scss';
 import './OlzEditableText/OlzEditableText.scss';
-import './OlzLocationMap/OlzLocationMap.scss';
 import './OlzPostingListItem/OlzPostingListItem.scss';
 
 export * from './OlzDefaultForm/OlzDefaultForm';
 export * from './OlzEditableText/OlzEditableText';
 export * from './OlzProgressBar/OlzProgressBar';
+export * from './OlzLocationMap/OlzLocationMap';
 

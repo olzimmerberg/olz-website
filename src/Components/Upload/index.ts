fd7c3e399fccbd640a00f211b7af72637fd22062
Simon Hatt <hatt@google.com>
2023-12-10 22:07:27 +0100

    Remove multi file / image uploader (#1304)

--- src/Components/Upload/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './OlzMultiFileField/OlzMultiFileField';
-export * from './OlzMultiFileUploader/OlzMultiFileUploader';
-export * from './OlzMultiImageUploader/OlzMultiImageUploader';
-export * from './OlzUploadFile/OlzUploadFile';
-export * from './OlzUploadImage/OlzUploadImage';

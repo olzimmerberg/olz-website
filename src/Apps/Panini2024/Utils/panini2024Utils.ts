fa9ab61ed1f10917c7f8204b83f617170be98adc
Simon Hatt <hatt@google.com>
2023-12-24 15:44:15 +0100

    Panini: Fix frontend as well

--- src/Apps/Panini2024/Utils/panini2024Utils.ts
+++ src/Apps/Panini2024/Utils/panini2024Utils.ts
@@ -1,4 +1,10 @@
 export const DPI = 900;
-export const PANINI_SHORT = 42.8; // mm (50.8mm, 4mm margin)
-export const PANINI_LONG = 59; // mm (70mm, 5.5mm margin)
 export const MM_PER_INCH = 25.4;
+
+// This is the version with the self-printed foldable label paper:
+// export const PANINI_SHORT = 42.8; // mm (50.8mm, 4mm margin)
+// export const PANINI_LONG = 59; // mm (70mm, 5.5mm margin)
+
+// This is the version with 4x4 pictures per A4, which need to be cut:
+export const PANINI_SHORT = 45; // mm
+export const PANINI_LONG = 65; // mm

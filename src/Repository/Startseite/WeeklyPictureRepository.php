edb948b734cd88e2bb7bac38d07c338389fc5bc6
Simon Hatt <hatt@google.com>
2024-01-11 00:24:55 +0100

    Show multiple weekly pictures in carousel (#1330)

--- src/Repository/Startseite/WeeklyPictureRepository.php
+++ src/Repository/Startseite/WeeklyPictureRepository.php
@@ -5,14 +5,14 @@ namespace Olz\Repository\Startseite;
 use Doctrine\ORM\EntityRepository;
 
 class WeeklyPictureRepository extends EntityRepository {
-    public function getLatest() {
+    public function getLatestThree() {
         $dql = "
             SELECT wp
             FROM Olz\\Entity\\Startseite\\WeeklyPicture wp
             WHERE wp.on_off = 1
             ORDER BY wp.datum DESC
         ";
-        $query = $this->getEntityManager()->createQuery($dql)->setMaxResults(1);
-        return $query->getOneOrNullResult();
+        $query = $this->getEntityManager()->createQuery($dql)->setMaxResults(3);
+        return $query->getResult();
     }
 }
--- src/Startseite/Components/OlzWeeklyPictureTile/OlzWeeklyPictureTile.php
+++ src/Startseite/Components/OlzWeeklyPictureTile/OlzWeeklyPictureTile.php
@@ -41,30 +41,58 @@ class OlzWeeklyPictureTile extends AbstractOlzTile {
             ZZZZZZZZZZ;
         }
 
-        $out .= "<h2 class='weekly-picture-h2'>Bild der Woche {$mgmt_html}</h2>";
-        $out .= "<div class='weekly-picture-tile-container center'>";
-
         $entity_manager = $this->dbUtils()->getEntityManager();
+        $image_utils = ImageUtils::fromEnv();
 
         $weekly_picture_repo = $entity_manager->getRepository(WeeklyPicture::class);
-        $latest_weekly_picture = $weekly_picture_repo->getLatest();
+        $latest_weekly_pictures = $weekly_picture_repo->getLatestThree();
+        $index = 0;
+        $carousel_inner = '';
+        foreach ($latest_weekly_pictures as $weekly_picture) {
+            $text = $weekly_picture->getText();
+            $id = $weekly_picture->getId();
+            $image_id = $weekly_picture->getImageId();
+            $alternative_image_id = $weekly_picture->getAlternativeImageId();
 
-        if ($latest_weekly_picture) {
-            $text = $latest_weekly_picture->getText();
-            $id = $latest_weekly_picture->getId();
-            $image_id = $latest_weekly_picture->getImageId();
-            $alternative_image_id = $latest_weekly_picture->getAlternativeImageId();
-
-            $image_utils = ImageUtils::fromEnv();
+            $active_class = $index === 0 ? ' active' : '';
+            $carousel_inner .= "<div class='carousel-item{$active_class}'>";
             if (!$alternative_image_id) {
-                $out .= $image_utils->olzImage('weekly_picture', $id, $image_id, 512, 'image');
+                $carousel_inner .= $image_utils->olzImage('weekly_picture', $id, $image_id, 512, 'image');
             } else {
-                $out .= "<div class='lightgallery'><span onmouseover='olz.olzWeeklyPictureTileSwap()' id='olz-weekly-image'>".$image_utils->olzImage('weekly_picture', $id, $image_id, 512, 'gallery[weekly_picture]')."</span><span onmouseout='olz.olzWeeklyPictureTileUnswap()' id='olz-weekly-alternative-image'>".$image_utils->olzImage('weekly_picture', $id, $alternative_image_id, 512, 'gallery[weekly_picture]')."</span></div>";
+                $carousel_inner .= "<div class='lightgallery'><span onmouseover='olz.olzWeeklyPictureTileSwap()' id='olz-weekly-image'>".$image_utils->olzImage('weekly_picture', $id, $image_id, 512, 'gallery[weekly_picture]')."</span><span onmouseout='olz.olzWeeklyPictureTileUnswap()' id='olz-weekly-alternative-image'>".$image_utils->olzImage('weekly_picture', $id, $alternative_image_id, 512, 'gallery[weekly_picture]')."</span></div>";
             }
-            $out .= "<div class='weekly-picture-tile-text'>".$text."</div>";
+            $carousel_inner .= "<div class='weekly-picture-tile-text'>".$text."</div>";
+            $carousel_inner .= "</div>";
+            $index++;
         }
 
-        $out .= "</div>";
+        $out .= <<<ZZZZZZZZZZ
+        <h2 class='weekly-picture-h2'>Bild der Woche {$mgmt_html}</h2>
+        <div
+            id='weekly-picture-carousel'
+            class='carousel slide'
+            data-bs-ride='carousel'
+            data-bs-interval='15000'
+            data-bs-wrap="false"
+        >
+            <div class='carousel-inner'>{$carousel_inner}</div>
+            <button
+                class="carousel-control-prev"
+                data-bs-target="#weekly-picture-carousel"
+                data-bs-slide="prev"
+            >
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            </button>
+            <button
+                class="carousel-control-next"
+                data-bs-target="#weekly-picture-carousel"
+                data-bs-slide="next"
+            >
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            </button>
+        </div>
+        ZZZZZZZZZZ;
+
         return $out;
     }
 }

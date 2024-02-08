46e8cc3146d631a04a9268d6989c75f932cfb70e
Simon Hatt <hatt@google.com>
2024-02-08 01:59:26 +0100

    Bump npm versions (#1355)

--- _/index.test.ts
+++
@@ -1,8 +0,0 @@
-// This file is just here to cover all the files...
-export * from './index';
-
-describe('index.test.ts', () => {
-    it('ran', () => {
-        expect(true).toBe(true);
-    });
-});

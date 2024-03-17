c17e425fddcfff184df47e6f90fe748dcb1fc7df
Simon Hatt <hatt@google.com>
2024-03-17 15:45:10 +0100

    Update organigramm animation (#1405)

--- src/Roles/Components/OlzOrganigramm/OlzOrganigramm.ts
+++ src/Roles/Components/OlzOrganigramm/OlzOrganigramm.ts
@@ -1,8 +1,8 @@
-export function highlight_organigramm(id: string): void {
-    highlight_organigramm_scroll(id);
+export function highlightOrganigramm(id: string): void {
+    highlightOrganigrammScroll(id);
 }
 
-export function highlight_organigramm_scroll(id: string): void {
+export function highlightOrganigrammScroll(id: string): void {
     let elem = document.getElementById(id);
     if (elem && /box\\-[0-9]+\\-[0-9]+/.exec(elem.parentElement?.id ?? '')) {
         elem = elem.parentElement;
@@ -12,24 +12,14 @@ export function highlight_organigramm_scroll(id: string): void {
     }
     elem.style.backgroundColor = 'rgba(0,0,0,0)';
     const rect = elem.getBoundingClientRect();
-    const optimalPageYOffset = window.pageYOffset + rect.top + rect.height / 2 - window.innerHeight / 2;
-    const nextPageYOffset = window.pageYOffset + (optimalPageYOffset - window.pageYOffset) / 4;
-    if (nextPageYOffset <= 0) {
-        window.scrollTo(0, 0);
-        highlight_organigramm_color(id);
-    } else if (document.getElementsByTagName('body')[0].offsetHeight - window.innerHeight <= nextPageYOffset) {
-        window.scrollTo(0, document.getElementsByTagName('body')[0].offsetHeight - window.innerHeight);
-        highlight_organigramm_color(id);
-    } else if (Math.abs(nextPageYOffset - optimalPageYOffset) <= 3) {
-        window.scrollTo(0, optimalPageYOffset);
-        highlight_organigramm_color(id);
-    } else {
-        window.scrollTo(0, Math.round(nextPageYOffset));
-        window.setTimeout(() => { highlight_organigramm_scroll(id); }, 50);
-    }
+    const optimalPageYOffset = window.scrollY + rect.top + rect.height / 2 - window.innerHeight / 2;
+    window.scrollTo({top: optimalPageYOffset, behavior: 'smooth'});
+    window.setTimeout(() => {
+        highlightOrganigrammColor(id);
+    }, 200);
 }
 
-export function highlight_organigramm_color(id: string): void {
+export function highlightOrganigrammColor(id: string): void {
     let elem = document.getElementById(id);
     if (elem && /box\\-[0-9]+\\-[0-9]+/.exec(elem.parentElement?.id ?? '')) {
         elem = elem.parentElement;

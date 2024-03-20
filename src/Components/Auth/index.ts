05627789d5eaf76c49118cb14c4a91912470999d
Simon Hatt <hatt@google.com>
2024-03-21 00:41:11 +0100

    Migrate OlzResetPasswordModal, delete OlzSignUpModal

--- src/Components/Auth/index.ts
+++ src/Components/Auth/index.ts
@@ -1,7 +1,5 @@
 import './OlzAccountMenu/OlzAccountMenu.scss';
 import './OlzProfileForm/OlzProfileForm.scss';
-import './OlzResetPasswordModal/OlzResetPasswordModal.scss';
-import './OlzSignUpModal/OlzSignUpModal.scss';
 import './OlzUpdateUserAvatarModal/OlzUpdateUserAvatarModal.scss';
 import './OlzVerifyUserEmailModal/OlzVerifyUserEmailModal.scss';
 
@@ -14,6 +12,5 @@ export * from './OlzLoginModal/OlzLoginModal';
 export * from './OlzProfil/OlzProfil';
 export * from './OlzProfileForm/OlzProfileForm';
 export * from './OlzResetPasswordModal/OlzResetPasswordModal';
-export * from './OlzSignUpModal/OlzSignUpModal';
 export * from './OlzUpdateUserAvatarModal/OlzUpdateUserAvatarModal';
 export * from './OlzVerifyUserEmailModal/OlzVerifyUserEmailModal';

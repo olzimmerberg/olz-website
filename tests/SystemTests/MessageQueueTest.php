574ecff2516d2901a4f834542f4bef003b7de9eb
Simon Hatt <hatt@google.com>
2024-07-09 21:21:10 +0200

    Fix prod message queue system test (#1522)

--- tests/SystemTests/MessageQueueTest.php
+++ tests/SystemTests/MessageQueueTest.php
@@ -30,7 +30,37 @@ final class MessageQueueTest extends SystemTestCase {
     #[OnlyInModes(['prod'])]
     public function testMessageQueueOnProd(): void {
         $result = $this->runCommand('olz:test', null);
-        // TODO: Implement
-        $this->assertSame('', $result);
+
+        $last_on_continuously = $this->getThrottlingDateTime($result, 'on_continuously');
+        $this->assertNotNull($last_on_continuously);
+        $this->assertGreaterThan(new \DateTime('5 minutes 10 seconds ago'), $last_on_continuously);
+        $this->assertLessThanOrEqual(new \DateTime('now'), $last_on_continuously);
+
+        $last_on_daily = $this->getThrottlingDateTime($result, 'on_daily');
+        $this->assertNotNull($last_on_daily);
+        $this->assertGreaterThan(new \DateTime('1 day 2 hours ago'), $last_on_daily);
+        $this->assertLessThanOrEqual(new \DateTime('now'), $last_on_daily);
+
+        sleep(3); // give some time for the queue to process the message
+        $result = $this->runCommand('olz:test', null);
+
+        $last_test_message = $this->getThrottlingDateTime($result, 'test_message');
+        $this->assertNotNull($last_test_message);
+        $this->assertGreaterThan(new \DateTime('10 seconds ago'), $last_test_message);
+        $this->assertLessThanOrEqual(new \DateTime('now'), $last_test_message);
+    }
+
+    protected function getThrottlingDateTime(
+        string $test_command_output,
+        string $throttling_ident,
+    ): ?\DateTime {
+        $esc_ident = preg_quote($throttling_ident);
+        $pattern = "/\\s+{$esc_ident}\\: (\\d{4}-\\d{2}\\-\\d{2} \\d{2}\\:\\d{2}\\:\\d{2})\\s+/m";
+        $has_match = preg_match($pattern, $test_command_output, $matches);
+        if (!$has_match) {
+            return null;
+        }
+        $time = strtotime($matches[1]);
+        return is_int($time) ? new \DateTime(date('Y-m-d H:i:s', $time)) : null;
     }
 }

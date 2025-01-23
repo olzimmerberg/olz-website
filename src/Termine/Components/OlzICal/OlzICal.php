d3bea1847e2de5fce8f284952ec1d16deb958095
Simon Hatt <hatt@google.com>
2025-01-23 19:35:56 +0100

    Fix OlzIcal (#1631)

--- src/Termine/Components/OlzICal/OlzICal.php
+++ src/Termine/Components/OlzICal/OlzICal.php
@@ -8,12 +8,13 @@
 
 namespace Olz\Termine\Components\OlzICal;
 
+use Doctrine\Common\Collections\Criteria;
 use Olz\Components\Common\OlzComponent;
+use Olz\Entity\Termine\Termin;
 
 class OlzICal extends OlzComponent {
     /** @param array<string, mixed> $args */
     public function getHtml(array $args = []): string {
-        $db = $this->dbUtils()->getDb();
         $jahr = $this->dateUtils()->getCurrentDateInFormat('Y');
         $base_href = $this->envUtils()->getBaseHref();
         $code_href = $this->envUtils()->getCodeHref();
@@ -21,8 +22,16 @@ class OlzICal extends OlzComponent {
         $host = $this->envUtils()->getEmailForwardingHost();
 
         // Termine abfragen
-        $sql = "SELECT * FROM termine WHERE (start_date >= '{$jahr}-01-01') AND on_off=1";
-        $result = $db->query($sql);
+        $termin_repo = $this->entityManager()->getRepository(Termin::class);
+        $criteria = Criteria::create()
+            ->where(Criteria::expr()->andX(
+                Criteria::expr()->gte('start_date', new \DateTime("{$jahr}-01-01")),
+                Criteria::expr()->eq('on_off', 1),
+            ))
+            ->setFirstResult(0)
+            ->setMaxResults(1000000)
+        ;
+        $termine = $termin_repo->matching($criteria);
 
         // ical-Kalender
         $ical = "BEGIN:VCALENDAR".
@@ -34,70 +43,71 @@ class OlzICal extends OlzComponent {
         "\r\nX-WR-TIMEZONE:Europe/Zurich";
 
         // Termine
-        while ($row = $result->fetch_assoc()) {
-            $id = $row['id'];
-            $start_date = $row['start_date'];
-            $end_date = $row['end_date'];
-            $duration_days = (strtotime($end_date) - strtotime($start_date)) / 86400;
+        foreach ($termine as $termin) {
+            $id = $termin->getId();
+            $start_date = $termin->getStartDate();
+            $end_date = $termin->getEndDate();
+            $duration_days = $end_date ? ($end_date->getTimestamp() - $start_date->getTimestamp()) / 86400 : 0;
             $should_split = $duration_days > 8;
+            $solv_id = $termin->getSolvId();
 
-            // Links extrahieren
-            $links = $row['link'];
-            $dom = new \DOMDocument();
-            $dom->loadHTML($links ? $links : ' ');
-            $_links = "OLZ-Termin: {$base_href}{$code_href}termine/{$id}";
-            $_attach = "\r\nATTACH;FMTTYPE=text/html:{$base_href}{$code_href}termine/{$id}";
-            foreach ($dom->getElementsByTagName("a") as $a) {
-                $text = $a->textContent;
-                $url = $a->getAttribute("href");
-                $_links .= "\\n".$text.": ".$url;
-                $_attach .= "\r\nATTACH;FMTTYPE=text/html:".$url;
-            }
-            $_links .= ($row['solv_uid'] > 0) ? "\\nSOLV-Termin: https://www.o-l.ch/cgi-bin/fixtures?&mode=show&unique_id=".$row['solv_uid'] : "";
-            $_attach .= ($row['solv_uid'] > 0) ? "\r\nATTACH;FMTTYPE=text/html:https://www.o-l.ch/cgi-bin/fixtures?&mode=show&unique_id=".$row['solv_uid'] : "";
+            $olz_url = "{$base_href}{$code_href}termine/{$id}";
+            $solv_url = "https://www.o-l.ch/cgi-bin/fixtures?&mode=show&unique_id={$solv_id}";
+
+            $links = "OLZ-Termin: {$olz_url}";
+            $attach = "\r\nATTACH;FMTTYPE=text/html:{$olz_url}";
+            $links .= $solv_id ? "\nSOLV-Termin: {$solv_url}" : "";
+            $attach .= $solv_id ? "\r\nATTACH;FMTTYPE=text/html:{$solv_url}" : "";
 
             $start_date_fmt = $this->dateUtils()->olzDate('jjjjmmtt', $start_date);
-            $end_date_fmt = ($end_date > "0000-00-00") ? $this->dateUtils()->olzDate('jjjjmmtt', $end_date) : $start_date_fmt;
-            $modified_fmt = date('Ymd\THis\Z', strtotime($row['last_modified_at']));
-            $created_fmt = date('Ymd\THis\Z', strtotime($row['created_at']));
-            $description_fmt = str_replace("\r\n", "\\n", $row['text'])."\\n".$_links;
+            $end_date_fmt = $end_date ? $this->dateUtils()->olzDate('jjjjmmtt', $end_date) : $start_date_fmt;
+            $modified_fmt = $termin->getLastModifiedAt()->format('Ymd\THis\Z');
+            $created_fmt = $termin->getCreatedAt()->format('Ymd\THis\Z');
+            $description_fmt = $this->escapeText("{$termin->getText()}\n{$links}");
+            $label_idents = implode(', ', array_map(
+                fn ($label) => "{$label->getIdent()}",
+                [...$termin->getLabels()],
+            ));
             if ($should_split) {
                 $ical .=
-                "\r\nBEGIN:VEVENT\nDTSTART;VALUE=DATE:".$start_date_fmt.
-                "\r\nDTEND;VALUE=DATE:".$start_date_fmt.
-                "\r\nDTSTAMP:".$now_fmt.
-                "\r\nLAST-MODIFIED:".$modified_fmt.
-                "\r\nCREATED:".$created_fmt.
-                "\r\nSUMMARY:".$row['title']." (Beginn)".
-                "\r\nDESCRIPTION:".$description_fmt.
-                "\r\nCATEGORIES:".$row['typ'].
-                $_attach.
+                "\r\nBEGIN:VEVENT".
+                "\r\nDTSTART;VALUE=DATE:{$start_date_fmt}".
+                "\r\nDTEND;VALUE=DATE:{$start_date_fmt}".
+                "\r\nDTSTAMP:{$now_fmt}".
+                "\r\nLAST-MODIFIED:{$modified_fmt}".
+                "\r\nCREATED:{$created_fmt}".
+                "\r\nSUMMARY:{$termin->getTitle()} (Beginn)".
+                "\r\nDESCRIPTION:{$description_fmt}".
+                "\r\nCATEGORIES:{$label_idents}".
+                $attach.
                 "\r\nCLASS:PUBLIC".
                 "\r\nUID:olz_termin_{$id}_start@{$host}".
                 "\r\nEND:VEVENT".
-                "\r\nBEGIN:VEVENT\nDTSTART;VALUE=DATE:".$end_date_fmt.
-                "\r\nDTEND;VALUE=DATE:".$end_date_fmt.
-                "\r\nDTSTAMP:".$now_fmt.
-                "\r\nLAST-MODIFIED:".$modified_fmt.
-                "\r\nCREATED:".$created_fmt.
-                "\r\nSUMMARY:".$row['title']." (Ende)".
-                "\r\nDESCRIPTION:".$description_fmt.
-                "\r\nCATEGORIES:".$row['typ'].
-                $_attach.
+                "\r\nBEGIN:VEVENT".
+                "\r\nDTSTART;VALUE=DATE:{$end_date_fmt}".
+                "\r\nDTEND;VALUE=DATE:{$end_date_fmt}".
+                "\r\nDTSTAMP:{$now_fmt}".
+                "\r\nLAST-MODIFIED:{$modified_fmt}".
+                "\r\nCREATED:{$created_fmt}".
+                "\r\nSUMMARY:{$termin->getTitle()} (Ende)".
+                "\r\nDESCRIPTION:{$description_fmt}".
+                "\r\nCATEGORIES:{$label_idents}".
+                $attach.
                 "\r\nCLASS:PUBLIC".
                 "\r\nUID:olz_termin_{$id}_end@{$host}".
                 "\r\nEND:VEVENT";
             } else {
                 $ical .=
-                "\r\nBEGIN:VEVENT\nDTSTART;VALUE=DATE:".$start_date_fmt.
-                "\r\nDTEND;VALUE=DATE:".$end_date_fmt.
-                "\r\nDTSTAMP:".$now_fmt.
-                "\r\nLAST-MODIFIED:".$modified_fmt.
-                "\r\nCREATED:".$created_fmt.
-                "\r\nSUMMARY:".$row['title'].
-                "\r\nDESCRIPTION:".$description_fmt.
-                "\r\nCATEGORIES:".$row['typ'].
-                $_attach.
+                "\r\nBEGIN:VEVENT".
+                "\r\nDTSTART;VALUE=DATE:{$start_date_fmt}".
+                "\r\nDTEND;VALUE=DATE:{$end_date_fmt}".
+                "\r\nDTSTAMP:{$now_fmt}".
+                "\r\nLAST-MODIFIED:{$modified_fmt}".
+                "\r\nCREATED:{$created_fmt}".
+                "\r\nSUMMARY:{$termin->getTitle()}".
+                "\r\nDESCRIPTION:{$description_fmt}".
+                "\r\nCATEGORIES:{$label_idents}".
+                $attach.
                 "\r\nCLASS:PUBLIC".
                 "\r\nUID:olz_termin_{$id}@{$host}".
                 "\r\nEND:VEVENT";
@@ -107,5 +117,9 @@ class OlzICal extends OlzComponent {
         $ical .= "\r\nEND:VCALENDAR";
 
         return $ical;
+    }
+
+    protected function escapeText(string $text): string {
+        return preg_replace("/(\r\n|\n|\r)/", "\\n", $text);
     }
 }

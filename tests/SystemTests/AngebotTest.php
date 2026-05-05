ee389c4381272577d722881a961a7f73731daab1
Simon Hatt <hatt@google.com>
2026-05-05 15:27:52 +0200

    Refactor AngebotTest to include conditional checks (#1903)
    * Refactor AngebotTest to include conditional checks
    
    * Refactor AngebotTest.php for code clarity

--- tests/SystemTests/AngebotTest.php
+++ tests/SystemTests/AngebotTest.php
@@ -20,44 +20,46 @@ final class AngebotTest extends SystemTestCase {
         $browser->get($this->getUrl());
         $this->screenshot('angebot');
 
-        $role_mailto_link = $this->getBrowserElement('#role-mailto a');
-        $this->assertNotNull($role_mailto_link);
-        $this->assertSame("#", strval($role_mailto_link->getAttribute('href')));
-        $this->assertSame(
-            "return olz.initOlzRoleInfoModal(18)",
-            strval($role_mailto_link->getAttribute('onclick'))
-        );
-        $this->assertSame("Ressort Karten", strval($role_mailto_link->getText()));
+        if ($this->isInModes(['dev_rw', 'dev'])) {
+            $role_mailto_link = $this->getBrowserElement('#role-mailto a');
+            $this->assertNotNull($role_mailto_link);
+            $this->assertSame("#", strval($role_mailto_link->getAttribute('href')));
+            $this->assertSame(
+                "return olz.initOlzRoleInfoModal(18)",
+                strval($role_mailto_link->getAttribute('onclick'))
+            );
+            $this->assertSame("Ressort Karten", strval($role_mailto_link->getText()));
 
-        $role_direct_link = $this->getBrowserElement('#role-direct a');
-        $this->assertNotNull($role_direct_link);
-        $this->assertSame("#", strval($role_direct_link->getAttribute('href')));
-        $this->assertSame(
-            "return olz.initOlzRoleInfoModal(18)",
-            strval($role_direct_link->getAttribute('onclick'))
-        );
-        $this->assertSame("Kartenverkauf", strval($role_direct_link->getText()));
+            $role_direct_link = $this->getBrowserElement('#role-direct a');
+            $this->assertNotNull($role_direct_link);
+            $this->assertSame("#", strval($role_direct_link->getAttribute('href')));
+            $this->assertSame(
+                "return olz.initOlzRoleInfoModal(18)",
+                strval($role_direct_link->getAttribute('onclick'))
+            );
+            $this->assertSame("Kartenverkauf", strval($role_direct_link->getText()));
 
-        $user_mailto_link = $this->getBrowserElement('#user-mailto a');
-        $this->assertNotNull($user_mailto_link);
-        $this->assertSame("#", strval($user_mailto_link->getAttribute('href')));
-        $this->assertMatchesRegularExpression(
-            "/^return olz\\.initOlzEmailModal\\(\"[A-Za-z0-9]+\"\\)$/",
-            strval($user_mailto_link->getAttribute('onclick'))
-        );
-        $this->assertSame("Karen Karten", strval($user_mailto_link->getText()));
+            $user_mailto_link = $this->getBrowserElement('#user-mailto a');
+            $this->assertNotNull($user_mailto_link);
+            $this->assertSame("#", strval($user_mailto_link->getAttribute('href')));
+            $this->assertMatchesRegularExpression(
+                "/^return olz\\.initOlzEmailModal\\(\"[A-Za-z0-9]+\"\\)$/",
+                strval($user_mailto_link->getAttribute('onclick'))
+            );
+            $this->assertSame("Karen Karten", strval($user_mailto_link->getText()));
 
-        $user_direct_link = $this->getBrowserElement('#user-direct a');
-        $this->assertNotNull($user_direct_link);
-        $this->assertSame("#", strval($user_direct_link->getAttribute('href')));
-        $this->assertMatchesRegularExpression(
-            "/^return olz\\.initOlzEmailModal\\(\"[A-Za-z0-9]+\"\\)$/",
-            strval($user_direct_link->getAttribute('onclick'))
-        );
-        $this->assertSame("E-Mail", strval($user_direct_link->getText()));
-
-        // TODO: Dummy assert
-        $this->assertDirectoryExists(__DIR__);
+            $user_direct_link = $this->getBrowserElement('#user-direct a');
+            $this->assertNotNull($user_direct_link);
+            $this->assertSame("#", strval($user_direct_link->getAttribute('href')));
+            $this->assertMatchesRegularExpression(
+                "/^return olz\\.initOlzEmailModal\\(\"[A-Za-z0-9]+\"\\)$/",
+                strval($user_direct_link->getAttribute('onclick'))
+            );
+            $this->assertSame("E-Mail", strval($user_direct_link->getText()));
+        } else {
+            // TODO: Dummy assert
+            $this->assertDirectoryExists(__DIR__);
+        }
     }
 
     protected function getUrl(): string {
